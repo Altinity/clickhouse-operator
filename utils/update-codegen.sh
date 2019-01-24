112a36f76ee0c92f68784b5b7d491e2d0f6e817a
Sergiy Chechun <ss@chechun.org>
2019-01-24 10:29:27 +0200

    Dev schechun (#1)
    * Add CRD parser and artifacts
    
    * parser update
    
    * typo correction
    
    * Added comments regarding implicit inheritance  of values from .deployment section
    
    * CRD parser implementation updates
    
    * clickhouse configuration: adding zookeeper section
    
    * Add CR object example
    
    * CRO parser: k8s objects generator updates
    
    * Minor improvements
    
    * code refactoring
    
    * Add vendor into codebase
    
    * Add Dockerfile of ClickHouse Operator
    
    * Add deps configs and vendored packages
    
    * Add codegen script
    
    * Clickhouse operator main and app packages
    
    * Add CRD APIs
    
    * Add custom clientset
    
    * Add ClickHouseInstallation resource controller
    
    * Runtime loop updates
    
    * Remove debug information
    
    * remove copy/paste typos
    
    * Readme update
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * dep locks update
    
    * Add CHI object example
    
    * YAML variables type fixes
    
    * Remove relocated example file
    
    * Update ClickHouseInstallation Resource API
    
    * CHI object parser integration with CHI controller
    
    * Release version update

---
+++ utils/update-codegen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
+
+vendor/k8s.io/code-generator/generate-groups.sh all \
+github.com/altinity/clickhouse-operator/pkg/client \
+github.com/altinity/clickhouse-operator/pkg/apis \
+"clickhouse.altinity.com:v1"
---
+++ vendor/golang.org/x/tools/internal/lsp/testdata/foo/foo.go
@@ -0,0 +1,24 @@
+package foo
+
+type StructFoo struct { //@item(StructFoo, "StructFoo", "struct{...}", "struct")
+	Value int //@item(Value, "Value", "int", "field")
+}
+
+// Pre-set this marker, as we don't have a "source" for it in this package.
+/* Error() */ //@item(Error, "Error()", "string", "method")
+
+func Foo() { //@item(Foo, "Foo()", "", "func")
+	var err error
+	err.Error() //@complete("E", Error)
+}
+
+func _() {
+	var sFoo StructFoo           //@complete("t", StructFoo)
+	if x := sFoo; x.Value == 1 { //@complete("V", Value),typdef("sFoo", StructFoo)
+		return
+	}
+}
+
+//@complete("", Foo, IntFoo, StructFoo)
+
+type IntFoo int //@item(IntFoo, "IntFoo", "int", "type")
