59cd821d9ff82d85e3feca34cd7507ef6b89fb2b
alz <alz@altinity.com>
2019-03-22 13:24:47 +0300

    Inline documentation for docker image

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Builds a dev docker image
+# Alter image tag in deployment spec in dev env in order to use it
+
 eval $(minikube docker-env) # comment for outside of minikube
 cat Dockerfile | envsubst | docker build -t altinity/clickhouse-operator:dev .
+
+# In order to push images to dockehub use those commands:
+#
+# docker login -u altinitybuilds
+# docker push altinity/clickhouse-operator:dev
