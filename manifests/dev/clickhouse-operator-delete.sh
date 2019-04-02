b4553f1e1d9bef9fe1ae815951c4b8307f3414e4
Vladislav Klimenko <sunsingerus@gmail.com>
2019-04-02 12:07:28 +0300

    fix dev clickhouse-operator delete script

--- manifests/dev/clickhouse-operator-delete.sh
+++ manifests/dev/clickhouse-operator-delete.sh
@@ -5,7 +5,7 @@ CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE:-dev}"
 if [[ "${CHOPERATOR_NAMESPACE}" == "kube-system" ]]; then
     echo "Default k8s namespace 'kube-system' must not be deleted"
     echo "Delete components only"
-    kubectl delete --namespace="${CHOPERATOR_NAMESPACE}" -f clickhouse-operator-install.yaml
+    kubectl delete --namespace="${CHOPERATOR_NAMESPACE}" -f <(cat clickhouse-operator-install-dev-template.yaml | CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE}" envsubst)
 else
     echo "Delete ClickHouse Operator namespace ${CHOPERATOR_NAMESPACE}"
     kubectl delete namespace "${CHOPERATOR_NAMESPACE}"
