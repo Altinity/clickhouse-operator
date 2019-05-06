c496d0a8ff598aa23600dc4af8965678fd448cc9
Vladislav Klimenko <sunsingerus@gmail.com>
2019-05-06 19:57:07 +0300

    zookeeper: make screate/delete scripts folder-agnostic

--- manifests/zookeeper/quick-start-persistent-volume/zookeeper-1-node-create.sh
+++ manifests/zookeeper/quick-start-persistent-volume/zookeeper-1-node-create.sh
@@ -2,5 +2,7 @@
 
 ZK_NAMESPACE="${ZK_NAMESPACE:-zoo1ns}"
 
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 kubectl create namespace "${ZK_NAMESPACE}"
-kubectl --namespace="${ZK_NAMESPACE}" apply -f zookeeper-1-node.yaml
+kubectl --namespace="${ZK_NAMESPACE}" apply -f "${CUR_DIR}/zookeeper-1-node.yaml"
--- manifests/zookeeper/quick-start-stateless/zookeeper-3-nodes-create.sh
+++ manifests/zookeeper/quick-start-stateless/zookeeper-3-nodes-create.sh
@@ -2,6 +2,8 @@
 
 ZK_NAMESPACE="${ZK_NAMESPACE:-zoo3ns}"
 
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 kubectl create namespace "${ZK_NAMESPACE}"
-kubectl --namespace="${ZK_NAMESPACE}" apply -f zookeeper-3-nodes.yaml
+kubectl --namespace="${ZK_NAMESPACE}" apply -f "${CUR_DIR}/zookeeper-3-nodes.yaml"
 
