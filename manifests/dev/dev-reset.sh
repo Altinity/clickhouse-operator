761a5b36c8c3912e6ca758007ba76d3229039b93
Vladislav Klimenko <sunsingerus@gmail.com>
2019-05-17 15:05:11 +0300

    dev: env scripts - make dev scripts folder-ignorant

--- manifests/dev/dev-reset.sh
+++ manifests/dev/dev-reset.sh
@@ -4,6 +4,5 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source ${CUR_DIR}/dev-config.sh
 
-echo "Reset dev env  via ${CHOPERATOR_NAMESPACE} namespace"
+echo "Reset namespace: ${CHOPERATOR_NAMESPACE}"
 ${CUR_DIR}/dev-delete.sh && ${CUR_DIR}/dev-install.sh
-
