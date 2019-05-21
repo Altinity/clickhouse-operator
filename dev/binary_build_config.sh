c76619fc75deb27dbd3ae6009ae824362520e7c8
Vladislav Klimenko <sunsingerus@gmail.com>
2019-05-21 11:56:40 +0300

    env: Docker build image should use unified builder

--- dev/binary_build_config.sh
+++ dev/binary_build_config.sh
@@ -4,4 +4,6 @@
 
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SRC_ROOT=$(realpath ${CUR_DIR}/..)
-OPERATOR_BIN=${CUR_DIR}/clickhouse-operator
+
+# Operator binary can be specified externally
+OPERATOR_BIN=${OPERATOR_BIN:-${CUR_DIR}/clickhouse-operator}
