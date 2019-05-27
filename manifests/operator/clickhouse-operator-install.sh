b7fdae10cc4a3e2bb66b47480b95ba3e7b21299f
Vladislav Klimenko <sunsingerus@gmail.com>
2019-05-27 15:26:10 +0300

    env: operator manifest build script

--- manifests/operator/clickhouse-operator-install.sh
+++ manifests/operator/clickhouse-operator-install.sh
@@ -2,11 +2,23 @@
 
 CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE:-kube-system}"
 
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "Setup ClickHouse Operator into ${CHOPERATOR_NAMESPACE} namespace"
 
+echo "1. Build manifest"
+CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE}" ${CUR_DIR}/build-clickhouse-operator-yaml.sh
+
 # Let's setup all clickhouse-operator-related stuff into dedicated namespace
-kubectl create namespace "${CHOPERATOR_NAMESPACE}"
+if [[ "${CHOPERATOR_NAMESPACE}" == "kube-system" ]]; then
+    echo "2. No need to create kube-system namespace"
+elif [[ kubectl get namespace ${CHOPERATOR_NAMESPACE} ]]; then
+    echo "2. Namespace ${CHOPERATOR_NAMESPACE}  already exists"
+else
+    echo "2. Create namespace"
+    kubectl create namespace "${CHOPERATOR_NAMESPACE}"
+fi
 
 # Setup into dedicated namespace
+echo "3. Install operator into ${CHOPERATOR_NAMESPACE} namespace"
 kubectl apply --namespace="${CHOPERATOR_NAMESPACE}" -f clickhouse-operator-install.yaml
