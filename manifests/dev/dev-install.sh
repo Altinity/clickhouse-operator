6124c38d253583d9996a8d9327b0faa612a07a79
Vladislav Klimenko <sunsingerus@gmail.com>
2019-05-29 23:53:07 +0300

    env: dev manifests enhancement - write image and image pull policy

--- manifests/dev/dev-install.sh
+++ manifests/dev/dev-install.sh
@@ -8,14 +8,15 @@ echo "Create ${CHOPERATOR_NAMESPACE} namespace"
 kubectl create namespace "${CHOPERATOR_NAMESPACE}"
 
 if [[ "${INSTALL_FROM_ALTINITY_RELEASE_DOCKERHUB}" == "yes" ]]; then
-    # Full dev install in k8s
     kubectl -n "${CHOPERATOR_NAMESPACE}" apply -f <(CHOPERATOR_IMAGE="${CHOPERATOR_IMAGE}" CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE}" ${CUR_DIR}/cat-clickhouse-operator-yaml.sh)
 
     # Installation done
     exit $?
 else
-    # Dev install from all components
     echo "Install operator requirements"
+    echo "CHOPERATOR_NAMESPACE=${CHOPERATOR_NAMESPACE}"
+    echo "CHOPERATOR_IMAGE=${CHOPERATOR_IMAGE}"
+
     kubectl -n "${CHOPERATOR_NAMESPACE}" apply -f <(CHOPERATOR_IMAGE="${CHOPERATOR_IMAGE}" CHOPERATOR_NAMESPACE="${CHOPERATOR_NAMESPACE}" MANIFEST_PRINT_DEPLOYMENT="no" ${CUR_DIR}/cat-clickhouse-operator-yaml.sh)
 
     if [[ "${INSTALL_FROM_DEPLOYMENT_MANIFEST}" == "yes" ]]; then
