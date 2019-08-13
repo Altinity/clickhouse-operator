6d4d7528f53b5ee1f2b0da8a82d694a04cb179c3
Vladislav Klimenko <sunsingerus@gmail.com>
2019-08-13 15:06:17 +0300

    dev: use kubernetes version 1.13.7 - latest supported by EKS

--- dev/update-codegen.sh
+++ dev/update-codegen.sh
@@ -7,10 +7,15 @@ set -o nounset
 # Only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
-PROJECT_ROOT="$(dirname "${BASH_SOURCE}")/.."
-CODEGEN_PKG="${CODEGEN_PKG:-$(cd "${PROJECT_ROOT}"; ls -d -1 "${PROJECT_ROOT}/vendor/k8s.io/code-generator" 2>/dev/null || echo "${GOPATH}/src/k8s.io/code-generator")}"
+PROJECT_ROOT=$(realpath "$(dirname "${BASH_SOURCE}")/..")
+CODEGEN_PKG=$(realpath "${CODEGEN_PKG:-$(cd "${PROJECT_ROOT}"; ls -d -1 "${PROJECT_ROOT}/vendor/k8s.io/code-generator" 2>/dev/null || echo "${GOPATH}/src/k8s.io/code-generator")}")
 
-"${PROJECT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh" all \
+#echo "Generating code with the following options:"
+#echo "PROJECT_ROOT=${PROJECT_ROOT}"
+#echo "CODEGEN_PKG==${CODEGEN_PKG}"
+
+"${PROJECT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh" \
+    all \
     github.com/altinity/clickhouse-operator/pkg/client \
     github.com/altinity/clickhouse-operator/pkg/apis \
     "clickhouse.altinity.com:v1"
