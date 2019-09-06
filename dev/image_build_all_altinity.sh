dd67d2095626984e588a2ee360a625ee5c76310b
Vladislav Klimenko <sunsingerus@gmail.com>
2019-09-06 12:53:40 +0300

    env: image build - ALL scrtipts

--- dev/image_build_all_altinity.sh
+++ dev/image_build_all_altinity.sh
@@ -7,4 +7,4 @@
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 "${CUR_DIR}"/image_build_metrics_exporter_altinity.sh
-"${CUR_DIR}"imageo_build_operator_altinity.sh
+"${CUR_DIR}"/image_build_operator_altinity.sh
