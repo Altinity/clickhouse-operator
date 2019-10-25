48ba13246d7c0178a3b0ac8a2227e35640095cab
Vladislav Klimenko <sunsingerus@gmail.com>
2019-10-25 12:53:42 +0300

    dev: metrcis fetcher - fetch functions unification

--- pkg/apis/metrics/clickhouse_fetcher.go
+++ pkg/apis/metrics/clickhouse_fetcher.go
@@ -15,6 +15,7 @@
 package metrics
 
 import (
+	sqlmodule "database/sql"
 	"github.com/MakeNowJust/heredoc"
 
 	"github.com/altinity/clickhouse-operator/pkg/model/clickhouse"
@@ -26,7 +27,8 @@ const (
 		database,
 		table,
 		toString(is_session_expired) AS is_session_expired
-	FROM system.replicas`
+	FROM system.replicas
+	`
 
 	queryMetricsSQL = `
     SELECT
@@ -51,45 +53,46 @@ const (
     FROM system.events
     UNION ALL
     SELECT 
-        'metric.DiskDataBytes'    AS metric,
-        toString(sum(bytes_on_disk)) AS value,
+        'metric.DiskDataBytes'                      AS metric,
+        toString(sum(bytes_on_disk))                AS value,
         'Total data size for all ClickHouse tables' AS description,
-	'gauge'                   AS type
+	    'gauge'                                     AS type
     FROM system.parts
     UNION ALL
     SELECT 
-        'metric.MemoryPrimaryKeyBytesAllocated' AS metric,
+        'metric.MemoryPrimaryKeyBytesAllocated'              AS metric,
         toString(sum(primary_key_bytes_in_memory_allocated)) AS value,
-        'Memory size allocated for primary keys' AS description,
-        'gauge'                   AS type
+        'Memory size allocated for primary keys'             AS description,
+        'gauge'                                              AS type
     FROM system.parts
     UNION ALL
     SELECT 
-        'metric.MemoryDictionaryBytesAllocated' AS metric,
-        toString(sum(bytes_allocated)) AS value,
+        'metric.MemoryDictionaryBytesAllocated'  AS metric,
+        toString(sum(bytes_allocated))           AS value,
         'Memory size allocated for dictionaries' AS description,
-        'gauge'                   AS type
+        'gauge'                                  AS type
     FROM system.dictionaries
     UNION ALL
     SELECT 
-        'metric.DiskFreeBytes'    AS metric,
-        toString(filesystemFree()) AS value,
+        'metric.DiskFreeBytes'                     AS metric,
+        toString(filesystemFree())                 AS value,
         'Free disk space available at file system' AS description,
-        'gauge'                   AS type
+        'gauge'                                    AS type
 	`
 
 	queryTableSizesSQL = `
 	SELECT
 		database,
 		table, 
-		toString(uniq(partition)) AS partitions, 
-		toString(count())         AS parts, 
-		toString(sum(bytes))      AS bytes, 
+		toString(uniq(partition))              AS partitions, 
+		toString(count())                      AS parts, 
+		toString(sum(bytes))                   AS bytes, 
 		toString(sum(data_uncompressed_bytes)) AS uncompressed_bytes, 
-		toString(sum(rows))       AS rows 
+		toString(sum(rows))                    AS rows 
 	FROM system.parts
 	WHERE active = 1
-	GROUP BY database, table`
+	GROUP BY database, table
+	`
 )
 
 type ClickHouseFetcher struct {
@@ -112,9 +115,7 @@ func (f *ClickHouseFetcher) newConn() *clickhouse.Conn {
 	return clickhouse.New(f.Hostname, f.Username, f.Password, f.Port)
 }
 
-// clickHouseQueryMetrics requests metrics data from the ClickHouse database using REST interface
-// data is a concealed output
-func (f *ClickHouseFetcher) clickHouseQueryMetrics() ([][]string, error) {
+func (f *ClickHouseFetcher) clickHouseQueryMetricsOld() ([][]string, error) {
 	data := make([][]string, 0)
 	conn := f.newConn()
 	rows, err := conn.Query(heredoc.Doc(queryMetricsSQL))
@@ -130,9 +131,21 @@ func (f *ClickHouseFetcher) clickHouseQueryMetrics() ([][]string, error) {
 	return data, nil
 }
 
-// clickHouseQueryTableSizes requests data sizes from the ClickHouse database using REST interface
-// data is a concealed output
-func (f *ClickHouseFetcher) clickHouseQueryTableSizes() ([][]string, error) {
+// clickHouseQueryMetrics requests metrics data from the ClickHouse database
+func (f *ClickHouseFetcher) clickHouseQueryMetrics() ([][]string, error) {
+	return f.clickHouseQueryScanRows(
+		heredoc.Doc(queryMetricsSQL),
+		func(rows *sqlmodule.Rows, data *[][]string) error {
+			var metric, value, description, _type string
+			if err := rows.Scan(&metric, &value, &description, &_type); err == nil {
+				*data = append(*data, []string{metric, value, description, _type})
+			}
+			return nil
+		},
+	)
+}
+
+func (f *ClickHouseFetcher) clickHouseQueryTableSizesOld() ([][]string, error) {
 	data := make([][]string, 0)
 	conn := f.newConn()
 	rows, err := conn.Query(heredoc.Doc(queryTableSizesSQL))
@@ -148,9 +161,21 @@ func (f *ClickHouseFetcher) clickHouseQueryTableSizes() ([][]string, error) {
 	return data, nil
 }
 
-// clickHouseQuerySystemReplicas requests replica information from the ClickHouse database using REST interface
-// data is a concealed output
-func (f *ClickHouseFetcher) clickHouseQuerySystemReplicas() ([][]string, error) {
+// clickHouseQueryTableSizes requests data sizes from the ClickHouse
+func (f *ClickHouseFetcher) clickHouseQueryTableSizes() ([][]string, error) {
+	return f.clickHouseQueryScanRows(
+		heredoc.Doc(queryTableSizesSQL),
+		func(rows *sqlmodule.Rows, data *[][]string) error {
+			var database, table, partitions, parts, bytes, uncompressed, _rows string
+			if err := rows.Scan(&database, &table, &partitions, &parts, &bytes, &uncompressed, &_rows); err == nil {
+				*data = append(*data, []string{database, table, partitions, parts, bytes, uncompressed, _rows})
+			}
+			return nil
+		},
+	)
+}
+
+func (f *ClickHouseFetcher) clickHouseQuerySystemReplicasOld() ([][]string, error) {
 	data := make([][]string, 0)
 	conn := f.newConn()
 	rows, err := conn.Query(heredoc.Doc(querySystemReplicasSQL))
@@ -161,6 +186,34 @@ func (f *ClickHouseFetcher) clickHouseQuerySystemReplicas() ([][]string, error) 
 		var database, table, isSessionExpired string
 		if err := rows.Scan(&database, &table, &isSessionExpired); err == nil {
 			data = append(data, []string{database, table, isSessionExpired})
+		}
+	}
+	return data, nil
+}
+
+// clickHouseQuerySystemReplicas requests replica information from the ClickHouse database
+func (f *ClickHouseFetcher) clickHouseQuerySystemReplicas() ([][]string, error) {
+	return f.clickHouseQueryScanRows(
+		heredoc.Doc(querySystemReplicasSQL),
+		func(rows *sqlmodule.Rows, data *[][]string) error {
+			var database, table, isSessionExpired string
+			if err := rows.Scan(&database, &table, &isSessionExpired); err == nil {
+				*data = append(*data, []string{database, table, isSessionExpired})
+			}
+			return nil
+		},
+	)
+}
+
+// clickHouseQueryScanRows scan all rows by external scan function
+func (f *ClickHouseFetcher) clickHouseQueryScanRows(sql string, scan func(rows *sqlmodule.Rows, data *[][]string) error) ([][]string, error) {
+	data := make([][]string, 0)
+	conn := f.newConn()
+	if rows, err := conn.Query(heredoc.Doc(querySystemReplicasSQL)); err != nil {
+		return nil, err
+	} else {
+		for rows.Next() {
+			_ = scan(rows, &data)
 		}
 	}
 	return data, nil
