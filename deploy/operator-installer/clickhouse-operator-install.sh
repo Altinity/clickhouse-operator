75d4b7147ec9f9353d3cf7ff95d3ff72c710d558
Vladislav Klimenko <sunsingerus@gmail.com>
2019-11-20 15:53:31 +0300

    env: prepare template and snippet for protected environments

--- deploy/operator-installer/clickhouse-operator-install.sh
+++ deploy/operator-installer/clickhouse-operator-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Namespace to install operator
+# Namespace to install operator into
 OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-clickhouse-operator}"
+# Namespace to install metrics-exporter into
 METRICS_EXPORTER_NAMESPACE="${OPERATOR_NAMESPACE}"
 
 # Operator's docker image
 OPERATOR_IMAGE="${OPERATOR_IMAGE:-altinity/clickhouse-operator:latest}"
+# Metrics exporter's docker image
 METRICS_EXPORTER_IMAGE="${METRICS_EXPORTER_IMAGE:-altinity/metrics-exporter:latest}"
 
 #
@@ -29,11 +31,53 @@ function is_curl_available() {
 }
 
 #
+# Check whether wget is available
+#
+function is_wget_available() {
+    if ! wget --version > /dev/null; then
+        echo "wget is unavailable, can not continue"
+        exit 1
+    fi
+}
+
+function check_file_getter_available() {
+    if curl --version > /dev/null; then
+        # curl is available - use it
+        :
+    elif wget --version > /dev/null; then
+        # wget is available - use it
+        :
+    else
+        echo "neither curl nor wget is unavailable, can not continue"
+        exit 1
+    fi
+}
+
+
+#
 # Check whether envsubst is available
 #
-function is_envsubst_available() {
+function checks_envsubst_available() {
     if ! envsubst --version > /dev/null; then
         echo "curl is unavailable, can not continue"
+        exit 1
+    fi
+}
+
+#
+# Get file
+#
+function get_file() {
+    local URL="$1"
+
+    if curl --version > /dev/null; then
+        # curl is available - use it
+        curl -s "${URL}"
+    elif wget --version > /dev/null; then
+        # wget is available - use it
+        wget -qO- "${URL}"
+    else
+        echo "neither curl nor wget is unavailable, can not continue"
         exit 1
     fi
 }
@@ -42,28 +86,27 @@ function is_envsubst_available() {
 ## Main
 ##
 
-is_curl_available
-is_kubectl_available
-is_envsubst_available
+check_file_getter_available
+checks_envsubst_available
 
-echo "Setup ClickHouse Operator into ${OPERATOR_NAMESPACE} namespace"
+echo "Setup ClickHouse Operator into '${OPERATOR_NAMESPACE}' namespace"
 
 if kubectl get namespace "${OPERATOR_NAMESPACE}" 1>/dev/null 2>/dev/null; then
-    echo "Namespace ${OPERATOR_NAMESPACE} already exists"
+    echo "Namespace '${OPERATOR_NAMESPACE}' already exists"
     if kubectl get deployment clickhouse-operator -n "${OPERATOR_NAMESPACE}" 1>/dev/null 2>/dev/null; then
-        echo "clickhouse-operator is already installed into ${OPERATOR_NAMESPACE} namespace. Abort."
+        echo "clickhouse-operator is already installed in '${OPERATOR_NAMESPACE}' namespace. Will not install new. Abort."
         exit 1
     else
-        echo "Looks like clickhouse-operator is not installed in ${OPERATOR_NAMESPACE} namespace. Going to install"
+        echo "Looks like clickhouse-operator is not installed in '${OPERATOR_NAMESPACE}' namespace. Going to install"
     fi
 else
-    echo "No ${OPERATOR_NAMESPACE} namespace found. Going to create"
+    echo "No '${OPERATOR_NAMESPACE}' namespace found. Going to create"
     kubectl create namespace "${OPERATOR_NAMESPACE}"
 fi
 
 # Setup clickhouse-operator into specified namespace
 kubectl apply --namespace="${OPERATOR_NAMESPACE}" -f <( \
-    curl -s https://raw.githubusercontent.com/Altinity/clickhouse-operator/master/deploy/operator/clickhouse-operator-install-template.yaml | \
+    get_file https://raw.githubusercontent.com/Altinity/clickhouse-operator/master/deploy/operator/clickhouse-operator-install-template.yaml | \
         OPERATOR_IMAGE="${OPERATOR_IMAGE}" \
         OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE}" \
         METRICS_EXPORTER_IMAGE="${METRICS_EXPORTER_IMAGE}" \
