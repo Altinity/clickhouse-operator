08715fcbd7e1ab56309a448205ae07140957c2f9
Vladislav Klimenko <sunsingerus@gmail.com>
2019-11-21 15:34:29 +0300

    env: grafana automation

--- deploy/grafana/install-grafana-operator.sh
+++ deploy/grafana/install-grafana-operator.sh
@@ -5,6 +5,7 @@ GRAFANA_NAMESPACE="${GRAFANA_NAMESPACE:-grafana}"
 function clean_dir() {
     DIR="$1"
 
+    echo "##############################"
     echo "Clean dir $DIR"
 }
 
@@ -18,7 +19,14 @@ TMP_DIR=$(mktemp -d)
 trap "clean_dir ${TMP_DIR}" SIGHUP SIGINT SIGQUIT SIGFPE SIGKILL SIGALRM SIGTERM
 
 GRAFANA_OPERATOR_DIR="${TMP_DIR}/grafana-operator"
-clean_dir "${GRAFANA_OPERATOR_DIR}"
+mkdir -p "${GRAFANA_OPERATOR_DIR}"
+
+if [[ ! -z "$(ls -A "${GRAFANA_OPERATOR_DIR}")" ]]; then
+     echo "${GRAFANA_OPERATOR_DIR} is not empty. Abort"
+     exit 1
+fi
+
+
 git clone "https://github.com/integr8ly/grafana-operator" "${GRAFANA_OPERATOR_DIR}"
 
 echo "Setup Grafana operator into ${GRAFANA_NAMESPACE} namespace"
@@ -37,15 +45,4 @@ kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f "${GRAFANA_OPERATOR_DIR}/dep
 # 4. Deploy operator itself
 kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f "${GRAFANA_OPERATOR_DIR}/deploy/operator.yaml"
 
-########################################
-##                                    ##
-## Install Grafana as Custom Resource ##
-##                                    ##
-########################################
-
-
-kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f "${GRAFANA_OPERATOR_DIR}/deploy/examples/Grafana.yaml"
-kubectl --namespace="${GRAFANA_NAMESPACE}" get ingress
-
 clean_dir "${TMP_DIR}"
-
---
+++ deploy/grafana/install-grafana-with-operator.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+GRAFANA_NAMESPACE="${GRAFANA_NAMESPACE:-grafana}"
+
+GRAFANA_NAME="${GRAFANA_NAME:-grafana}"
+GRAFANA_ADMIN_USER="${GRAFANA_ADMIN_USER:-admin}"
+GRAFANA_ADMIN_PASSWORD="${GRAFANA_ADMIN_PASSWORD:-admin}"
+GRAFANA_DISABLE_LOGIN_FORM="${GRAFANA_DISABLE_LOGIN_FORM:-False}"
+GRAFANA_DISABLE_SIGNOUT_MENU="${GRAFANA_DISABLE_SIGNOUT_MENU:-True}"
+
+GRAFANA_DATA_SOURCE_NAME="${GRAFANA_DATA_SOURCE_NAME:-chdatasource}"
+PROMETHEUS_URL="${PROMETHEUS_URL:-http://prometheus.prometheus:9090}"
+
+GRAFANA_DASHBOARD_NAME="${GRAFANA_DASHBOARD_NAME:-chdashboard}"
+
+########################################
+##                                    ##
+## Install Grafana as Custom Resource ##
+##                                    ##
+########################################
+
+
+kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f <( \
+    cat grafana-cr-template.yaml | \
+    GRAFANA_NAME="$GRAFANA_NAME" \
+    GRAFANA_ADMIN_USER="$GRAFANA_ADMIN_USER" \
+    GRAFANA_ADMIN_PASSWORD="$GRAFANA_ADMIN_PASSWORD" \
+    GRAFANA_DISABLE_LOGIN_FORM="$GRAFANA_DISABLE_LOGIN_FORM" \
+    GRAFANA_DISABLE_SIGNOUT_MENU="$GRAFANA_DISABLE_SIGNOUT_MENU" \
+    envsubst \
+)
+
+echo "Waiting to start"
+sleep 60
+
+kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f <( \
+    cat grafana-data-source-cr-template.yaml | \
+    GRAFANA_DATA_SOURCE_NAME="$GRAFANA_DATA_SOURCE_NAME" \
+    PROMETHEUS_URL="$PROMETHEUS_URL" \
+    envsubst \
+)
+
+#echo "Waiting to start"
+#sleep 60
+#
+#kubectl apply --namespace="${GRAFANA_NAMESPACE}" -f <( \
+#    cat grafana-dashboard-cr-template.yaml | \
+#    GRAFANA_DASHBOARD_NAME="$GRAFANA_DASHBOARD_NAME" \
+#    envsubst \
+#)
