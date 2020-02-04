d8d9eca40fc105f0f3f3c8e8120f659ccea57c2b
Vladislav Klimenko <sunsingerus@gmail.com>
2020-02-04 11:43:27 +0300

    env: support new verification constant for shortcut

--- deploy/dev/dev-watch.sh
+++ deploy/dev/dev-watch.sh
@@ -4,5 +4,11 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source "${CUR_DIR}/dev-config.sh"
 
-watch -n0 "kubectl -n ${OPERATOR_NAMESPACE} get chi,chit -o wide; echo ''; kubectl -n ${OPERATOR_NAMESPACE} get pod,service,configmap,pv,statefulset,pvc"
+watch -n0 " \
+	kubectl -n ${OPERATOR_NAMESPACE} get chi,chit -o wide; \
+	echo ''; \
+	kubectl -n ${OPERATOR_NAMESPACE} get pod -o wide; \
+	echo ''; \
+	kubectl -n ${OPERATOR_NAMESPACE} get service,configmap,pv,statefulset,pvc; \
+"
 
