8cceab70c39251efc3deb74a76e40819a88b4a63
Vladislav Klimenko <sunsingerus@gmail.com>
2020-03-03 19:57:07 +0300

    dev: code generator

--- dev/run_code_generator.sh
+++ dev/run_code_generator.sh
@@ -11,13 +11,13 @@ set -o pipefail
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${CUR_DIR}/go_build_config.sh"
 
-CODEGEN_PKG=$(realpath "${CODEGEN_PKG:-$(cd "${SRC_ROOT}"; ls -d -1 "${SRC_ROOT}/${MODULES_DIR}/k8s.io/code-generator" 2>/dev/null || echo "${GOPATH}/src/k8s.io/code-generator")}")
+CODE_GENERATOR_DIR=$(realpath "${CODE_GENERATOR_DIR:-$(cd "${SRC_ROOT}"; ls -d -1 "${SRC_ROOT}/${MODULES_DIR}/k8s.io/code-generator" 2>/dev/null || echo "${GOPATH}/src/k8s.io/code-generator")}")
 
 #echo "Generating code with the following options:"
 #echo "PROJECT_ROOT=${PROJECT_ROOT}"
 #echo "CODEGEN_PKG==${CODEGEN_PKG}"
 
-bash "${CODEGEN_PKG}/generate-groups.sh" \
+bash "${CODE_GENERATOR_DIR}/generate-groups.sh" \
     all \
     github.com/altinity/clickhouse-operator/pkg/client \
     github.com/altinity/clickhouse-operator/pkg/apis \
