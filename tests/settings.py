188b8dba6d3fe3b3fafedea38fa4871f20f2b7dd
alex-zaitsev <alz@altinity.com>
2020-04-14 12:31:05 +0300

    Update settings.py
    Leave the way to specify version explicitly

--- tests/settings.py
+++ tests/settings.py
@@ -3,6 +3,7 @@ import kubectl
 import pathlib
 
 version = open(os.path.join(pathlib.Path(__file__).parent.absolute(),"../release")).read(1024)
+# version = "0.9.7" 
 test_namespace = "test"
 clickhouse_template = "templates/tpl-clickhouse-stable.yaml"
 # clickhouse_template = "templates/tpl-clickhouse-19.11.yaml"
