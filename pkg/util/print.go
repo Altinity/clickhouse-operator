b99b5d2aa008cb95c1bb33690d8502c745747a05
Vladislav Klimenko <sunsingerus@gmail.com>
2020-05-06 10:22:06 +0300

    dev: simplify indented writer

--- pkg/util/print.go
+++ pkg/util/print.go
@@ -22,16 +22,13 @@ import (
 
 // Iline writes indented line with \n into provided io.Writer
 func Iline(w io.Writer, indent int, format string, a ...interface{}) {
-	if indent == 0 {
-		Fprintf(w, format, a...)
-		Fprintf(w, "\n")
-	} else {
-		// %16s
+	if indent > 0 {
+		// Prepare indentation template %16s
 		template := "%" + strconv.Itoa(indent) + "s"
 		Fprintf(w, template, " ")
-		Fprintf(w, format, a...)
-		Fprintf(w, "\n")
 	}
+	Fprintf(w, format, a...)
+	Fprintf(w, "\n")
 }
 
 // Fprintf suppresses warning for unused returns of fmt.Fprintf()
