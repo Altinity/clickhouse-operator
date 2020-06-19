ccee580cedd7b6ca40be93d96af042fec96deebe
Vladislav Klimenko <sunsingerus@gmail.com>
2020-06-19 18:04:38 +0300

    env: treat vars

--- dev/community-operators-crd-diff.sh
+++ dev/community-operators-crd-diff.sh
@@ -6,6 +6,6 @@ source "${CUR_DIR}/go_build_config.sh"
 
 CO_PATH=~/dev/community-operators/upstream-community-operators/clickhouse/0.10.0
 
-meld ${MANIFESTS_ROOT}/dev/clickhouse-operator-install-yaml-template-01-section-crd-01-chi.yaml $CO_PATH/clickhouseinstallations.clickhouse.altinity.com.crd.yaml
-meld ${MANIFESTS_ROOT}/dev/clickhouse-operator-install-yaml-template-01-section-crd-02-chit.yaml $CO_PATH/clickhouseinstallationtemplates.clickhouse.altinity.com.crd.yaml
-meld ${MANIFESTS_ROOT}/dev/clickhouse-operator-install-yaml-template-01-section-crd-03-chopconf.yaml $CO_PATH/clickhouseoperatorconfigurations.clickhouse.altinity.com.crd.yaml
+meld "${MANIFESTS_ROOT}"/dev/clickhouse-operator-install-yaml-template-01-section-crd-01-chi.yaml      "${CO_PATH}"/clickhouseinstallations.clickhouse.altinity.com.crd.yaml
+meld "${MANIFESTS_ROOT}"/dev/clickhouse-operator-install-yaml-template-01-section-crd-02-chit.yaml     "${CO_PATH}"/clickhouseinstallationtemplates.clickhouse.altinity.com.crd.yaml
+meld "${MANIFESTS_ROOT}"/dev/clickhouse-operator-install-yaml-template-01-section-crd-03-chopconf.yaml "${CO_PATH}"/clickhouseoperatorconfigurations.clickhouse.altinity.com.crd.yaml
