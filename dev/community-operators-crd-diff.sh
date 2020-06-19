f8c12ae4f3e6a1d3a0f6dd7c7ec6ebe313ace4aa
Vladislav Klimenko <sunsingerus@gmail.com>
2020-06-19 18:05:52 +0300

    env: use version instead of predefined

--- dev/community-operators-crd-diff.sh
+++ dev/community-operators-crd-diff.sh
@@ -4,7 +4,7 @@
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${CUR_DIR}/go_build_config.sh"
 
-CO_PATH=~/dev/community-operators/upstream-community-operators/clickhouse/0.10.0
+CO_PATH=~/dev/community-operators/upstream-community-operators/clickhouse/${VERSION}
 
 meld "${MANIFESTS_ROOT}"/dev/clickhouse-operator-install-yaml-template-01-section-crd-01-chi.yaml      "${CO_PATH}"/clickhouseinstallations.clickhouse.altinity.com.crd.yaml
 meld "${MANIFESTS_ROOT}"/dev/clickhouse-operator-install-yaml-template-01-section-crd-02-chit.yaml     "${CO_PATH}"/clickhouseinstallationtemplates.clickhouse.altinity.com.crd.yaml
