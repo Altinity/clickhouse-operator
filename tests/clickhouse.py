d4db3c8926f6d0ef82078a8ec5fb439ed9daf538
Vladislav Klimenko <sunsingerus@gmail.com>
2020-09-04 13:48:03 +0300

    tests: format clickhouse functions

--- tests/clickhouse.py
+++ tests/clickhouse.py
@@ -2,7 +2,17 @@ import kubectl
 import settings
 
 
-def query(chi_name, sql, with_error=False, host="127.0.0.1", port="9000", user="default", pwd="", ns=settings.test_namespace, timeout=60):
+def query(
+        chi_name,
+        sql,
+        with_error=False,
+        host="127.0.0.1",
+        port="9000",
+        user="default",
+        pwd="",
+        ns=settings.test_namespace,
+        timeout=60,
+):
     pod_names = kubectl.get_pod_names(chi_name, ns)
     pod_name = pod_names[0]
     for p in pod_names:
@@ -12,10 +22,33 @@ def query(chi_name, sql, with_error=False, host="127.0.0.1", port="9000", user="
     pwd_str = "" if pwd == "" else f"--password={pwd}"
 
     if with_error:
-        return kubectl.launch(f"exec {pod_name} -n {ns} -- clickhouse-client -mn -h {host} --port={port} -u {user} {pwd_str} --query=\"{sql}\" 2>&1", ok_to_fail=True, timeout=timeout)
+        return kubectl.launch(
+            f"exec {pod_name} -n {ns}"
+            f" -- "
+            f"clickhouse-client -mn -h {host} --port={port} -u {user} {pwd_str} "
+            f"--query=\"{sql}\""
+            f" 2>&1",
+            timeout=timeout,
+            ok_to_fail=True,
+        )
     else:
-        return kubectl.launch(f"exec {pod_name} -n {ns} -- clickhouse-client -mn -h {host} --port={port} -u {user} {pwd_str} --query=\"{sql}\"", timeout=timeout)
+        return kubectl.launch(
+            f"exec {pod_name} -n {ns}"
+            f" -- "
+            f"clickhouse-client -mn -h {host} --port={port} -u {user} {pwd_str} "
+            f"--query=\"{sql}\"",
+            timeout=timeout,
+        )
 
 
-def query_with_error(chi_name, sql, host="127.0.0.1", port="9000", user="default", pwd="", ns=settings.test_namespace, timeout=60):
+def query_with_error(
+        chi_name,
+        sql,
+        host="127.0.0.1",
+        port="9000",
+        user="default",
+        pwd="",
+        ns=settings.test_namespace,
+        timeout=60,
+):
     return query(chi_name, sql, True, host, port, user, pwd, ns, timeout)
