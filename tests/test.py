d4569c41c26ed820a7a884c476d9c14fc9205c64
Vladislav Klimenko <sunsingerus@gmail.com>
2021-07-09 01:24:45 +0300

    test: move test 21

--- tests/test.py
+++ tests/test.py
@@ -62,7 +62,7 @@ if main():
                 # test_operator.test_018, # Obsolete, covered by test_016
                 test_operator.test_019,
                 test_operator.test_020,
-                test_operator.test_021,  # this should go last while failing
+                test_operator.test_021,
                 test_operator.test_022,
                 test_operator.test_023,
                 test_operator.test_024,
