67e2231f610006781cb0e09e89e688143e7b2594
Vladislav Klimenko <sunsingerus@gmail.com>
2021-08-05 16:28:04 +0300

    env: dockerfiles templating

--- dev/go_build_all.sh
+++ dev/go_build_all.sh
@@ -5,7 +5,9 @@
 
 # Source configuration
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PROJECT_ROOT="$(realpath "${CUR_DIR}/..")"
 
+"${PROJECT_ROOT}"/dockerfile/build-docker-files.sh
 "${CUR_DIR}"/go_build_metrics_exporter.sh
 "${CUR_DIR}"/go_build_operator.sh
 "${CUR_DIR}"/run_gocard.sh
