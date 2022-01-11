bc2e1cc4ee4486faa5bf0870cad9d55ee15bc2c5
Vladislav Klimenko <sunsingerus@gmail.com>
2022-01-11 15:31:43 +0300

    env: support dev mode

--- deploy/dev/dev-config.sh
+++ deploy/dev/dev-config.sh
@@ -6,39 +6,19 @@ METRICS_EXPORTER_NAMESPACE="${OPERATOR_NAMESPACE}"
 # yes, no, none, dev, release, prod, latest
 DEPLOY_OPERATOR="${DEPLOY_OPERATOR:-no}"
 
-if  [[ "${DEPLOY_OPERATOR}" == "yes"     ]] || \
-    [[ "${DEPLOY_OPERATOR}" == "release" ]] || \
-    [[ "${DEPLOY_OPERATOR}" == "prod"    ]] || \
-    [[ "${DEPLOY_OPERATOR}" == "latest"  ]]
-then
-    # This would be release operator
-    OPERATOR_IMAGE="altinity/clickhouse-operator:latest"
-    METRICS_EXPORTER_IMAGE="altinity/metrics-exporter:latest"
-    DEPLOY_OPERATOR="yes"
-elif [[ "${DEPLOY_OPERATOR}" == "dev" ]]; then
-    # This would be dev operator
-    OPERATOR_IMAGE="sunsingerus/clickhouse-operator:dev"
-    METRICS_EXPORTER_IMAGE="sunsingerus/metrics-exporter:dev"
-    DEPLOY_OPERATOR="yes"
-elif [[ -z "${DEPLOY_OPERATOR}"         ]] || \
-     [[ "${DEPLOY_OPERATOR}" == "no"    ]] || \
-     [[  "${DEPLOY_OPERATOR}" == "none" ]]
-then
-    # Looks like no operator to be deployed
-    DEPLOY_OPERATOR="no"
-fi
+echo "DEPLOY_OPERATOR=${DEPLOY_OPERATOR}"
 
-if [[ ! -z "${OPERATOR_VERSION}" ]]; then
-    # Explicit operator version to be deployed
-    DEPLOY_OPERATOR="yes"
-fi
-
-# Verify DEPLOY_OPERATOR value
-if   [[ "${DEPLOY_OPERATOR}" == "yes" ]]; then
-    :
-elif [[ "${DEPLOY_OPERATOR}" == "no" ]]; then
-    :
-else
-    echo "Unclear, whether to install operator or not, abort"
-    exit 1
-fi
+case "${DEPLOY_OPERATOR}" in
+    "yes" | "release" | "prod" | "latest")
+        # This would be release operator
+        OPERATOR_IMAGE="altinity/clickhouse-operator:latest"
+        METRICS_EXPORTER_IMAGE="altinity/metrics-exporter:latest"
+        ;;
+    "dev")
+        # This would be dev operator
+        OPERATOR_IMAGE="sunsingerus/clickhouse-operator:dev"
+        METRICS_EXPORTER_IMAGE="sunsingerus/metrics-exporter:dev"
+        ;;
+    *)
+        ;;
+esac
