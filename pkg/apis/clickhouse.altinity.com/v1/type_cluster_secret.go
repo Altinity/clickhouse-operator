2a704266a5e68fbe2c890d50d269f8865760e4a6
Vladislav Klimenko <sunsingerus@gmail.com>
2022-09-19 13:09:00 +0300

    dev: Auto type in cluster secret

--- pkg/apis/clickhouse.altinity.com/v1/type_cluster_secret.go
+++ pkg/apis/clickhouse.altinity.com/v1/type_cluster_secret.go
@@ -18,16 +18,56 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// ClusterSecret defines the shared secret to for nodes to authenticate each other with
+// ClusterSecret defines the shared secret for nodes to authenticate each other with
 type ClusterSecret struct {
+	Auto      StringBool           `json:"auto,omitempty"      yaml:"auto,omitempty"`
 	Value     string               `json:"value,omitempty"     yaml:"value,omitempty"`
 	ValueFrom *ClusterSecretSource `json:"valueFrom,omitempty" yaml:"valueFrom,omitempty"`
 }
 
+// ClusterSecretSource
 type ClusterSecretSource struct {
 	SecretKeyRef *corev1.SecretKeySelector `json:"secretKeyRef,omitempty" yaml:"secretKeyRef,omitempty"`
 }
 
+// ClusterSecretSourceName specifies name of the source where secret is provided
+type ClusterSecretSourceName string
+
+// Possible values for ClusterSecretSourceName secret sources
+const (
+	ClusterSecretSourcePlaintext   ClusterSecretSourceName = "plaintext"
+	ClusterSecretSourceSecretRef   ClusterSecretSourceName = "secret_ref"
+	ClusterSecretSourceAuto        ClusterSecretSourceName = "auto"
+	ClusterSecretSourceUnspecified ClusterSecretSourceName = ""
+)
+
+// Source returns name of the source where secret is provided
+func (s *ClusterSecret) Source() ClusterSecretSourceName {
+	if s == nil {
+		// No secret is specified at all
+		return ClusterSecretSourceUnspecified
+	}
+
+	if s.HasValue() {
+		// Secret has explicit value specified
+		return ClusterSecretSourcePlaintext
+	}
+
+	if s.HasSecretKeyRef() {
+		// Secret has SecretKeyRef specified
+		return ClusterSecretSourceSecretRef
+	}
+
+	if s.Auto.IsTrue() {
+		// Secret is auto-generated
+		return ClusterSecretSourceAuto
+	}
+
+	// No secret is specified at all
+	return ClusterSecretSourceUnspecified
+}
+
+// HasValue checks whether explicit plaintext value is specified
 func (s *ClusterSecret) HasValue() bool {
 	if s == nil {
 		return false
@@ -35,6 +75,7 @@ func (s *ClusterSecret) HasValue() bool {
 	return s.Value != ""
 }
 
+// GetSecretKeyRef gets SecretKeySelector (typically named as SecretKeyRef) or nil
 func (s *ClusterSecret) GetSecretKeyRef() *corev1.SecretKeySelector {
 	if s == nil {
 		return nil
@@ -45,30 +86,17 @@ func (s *ClusterSecret) GetSecretKeyRef() *corev1.SecretKeySelector {
 	return s.ValueFrom.SecretKeyRef
 }
 
+// HasSecretKeyRef checks whether SecretKeySelector (typically named as SecretKeyRef) is available
 func (s *ClusterSecret) HasSecretKeyRef() bool {
 	return s.GetSecretKeyRef() != nil
 }
 
-func (s *ClusterSecret) Get() (string, bool) {
-	// Sanity check
-	if s == nil {
-		return "", false
+// GetAutoSecretKeyRef gets SecretKeySelector (typically named as SecretKeyRef) of an auto-generated secret or nil
+func (s *ClusterSecret) GetAutoSecretKeyRef(name string) *corev1.SecretKeySelector {
+	return &corev1.SecretKeySelector{
+		LocalObjectReference: corev1.LocalObjectReference{
+			Name: name,
+		},
+		Key: "secret",
 	}
-
-	// Explicit value has priority
-	if s.Value != "" {
-		return s.Value, false
-	}
-
-	// From now on explicit value is empty, check for SecretSource
-
-	if s.ValueFrom == nil {
-		return "", false
-	}
-
-	if s.ValueFrom.SecretKeyRef == nil {
-		return "", false
-	}
-
-	return "", true
 }
