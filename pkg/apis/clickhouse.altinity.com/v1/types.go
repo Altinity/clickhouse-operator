2e32a1f8744e8d64cb2bd1e8f5be6c1ad160d767
Vladislav Klimenko <sunsingerus@gmail.com>
2022-10-28 16:40:03 +0300

    dev: reduce default ConfigMapPropagationTimeout to 10 seconds

--- pkg/apis/clickhouse.altinity.com/v1/types.go
+++ pkg/apis/clickhouse.altinity.com/v1/types.go
@@ -434,7 +434,7 @@ func (t *ChiReconciling) SetDefaults() *ChiReconciling {
 		return nil
 	}
 	t.Policy = ReconcilingPolicyUnspecified
-	t.ConfigMapPropagationTimeout = 60
+	t.ConfigMapPropagationTimeout = 10
 	t.Cleanup = NewChiCleanup().SetDefaults()
 	return t
 }
