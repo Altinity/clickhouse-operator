f5c572beb8fe87402c5b6897b812a44c41aa8d1f
Vladislav Klimenko <sunsingerus@gmail.com>
2022-12-14 21:28:33 +0300

    test: setup configurable minikube kubernetes version

--- tests/e2e/run_minikube_reset.sh
+++ tests/e2e/run_minikube_reset.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 NODES="${NODES:-"1"}"
-echo "Launching cluster with $NODES nodes"
+# The Kubernetes version that the minikube will use
+# Ex: v1.2.3, 'stable' for v1.23.3, 'latest' for v1.23.4-rc.0)
+KUBERNETES_VERSION="${KUBERNETES_VERSION:-"stable"}"
+echo "Launching kubernetes cluster version ${KUBERNETES_VERSION} with ${NODES} nodes"
 sleep 5
 
-minikube delete && docker system prune -f && minikube start --nodes="${NODES}" && k9s -c ns
-
+minikube delete && docker system prune -f && minikube start --kubernetes-version="${KUBERNETES_VERSION}" --nodes="${NODES}" && k9s -c ns
