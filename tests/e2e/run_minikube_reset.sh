27cc273dafcb8fe8c72bfdd1a4dcb65e0e6060f6
Vladislav Klimenko <sunsingerus@gmail.com>
2022-12-14 21:22:43 +0300

    test: fix typo in name

--- tests/e2e/run_minikube_reset.sh
+++ tests/e2e/run_minikube_reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NODES="${NODES:-"1"}"
-echo "Lauching cluster with $NODES nodes"
+echo "Launching cluster with $NODES nodes"
 sleep 5
 
 minikube delete && docker system prune -f && minikube start --nodes="${NODES}" && k9s -c ns
