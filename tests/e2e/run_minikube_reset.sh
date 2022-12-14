62db80af410d52dea60e8c3b12ad6d032c2cd866
Vladislav Klimenko <sunsingerus@gmail.com>
2022-12-14 21:29:59 +0300

    test: nodes clarification

--- tests/e2e/run_minikube_reset.sh
+++ tests/e2e/run_minikube_reset.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# The number of nodes to spin up. Defaults to 1.
 NODES="${NODES:-"1"}"
 # The Kubernetes version that the minikube will use
 # Ex: v1.2.3, 'stable' for v1.23.3, 'latest' for v1.23.4-rc.0)
 KUBERNETES_VERSION="${KUBERNETES_VERSION:-"stable"}"
-echo "Launching kubernetes cluster version ${KUBERNETES_VERSION} with ${NODES} nodes"
+
+echo "Launching kubernetes cluster."
+echo "k8s version: ${KUBERNETES_VERSION}"
+echo "nodes:       ${NODES}"
 sleep 5
 
 minikube delete && docker system prune -f && minikube start --kubernetes-version="${KUBERNETES_VERSION}" --nodes="${NODES}" && k9s -c ns
