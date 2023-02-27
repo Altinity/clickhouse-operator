e75961b8055905f9aac1e006c7e39b212b2b73e5
Slach <bloodjazman@gmail.com>
2023-02-27 08:44:39 +0400

    another fix https://github.com/Altinity/clickhouse-operator/issues/1116
    Signed-off-by: Slach <bloodjazman@gmail.com>

--- deploy/operator/clickhouse-operator-install.sh
+++ deploy/operator/clickhouse-operator-install.sh
@@ -2,6 +2,6 @@
 
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-"kube-system"}"
-OPERATOR_VERSION=$(cat "$CUR_DIR/../../release")
+OPERATOR_VERSION=${OPERATOR_VERSION:-$(cat "$CUR_DIR/../../release")}
 VALIDATE_YAML="${VALIDATE_YAML:-"true"}"
 MANIFEST="${CUR_DIR}/clickhouse-operator-install-template.yaml" ${CUR_DIR}/../operator-web-installer/clickhouse-operator-install.sh
