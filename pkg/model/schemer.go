4261509ff3068ff84b69eac1aa6af9971f9e54bc
Vladislav Klimenko <sunsingerus@gmail.com>
2023-04-21 22:59:30 +0300

    dev: getter in schemer

--- pkg/model/schemer.go
+++ pkg/model/schemer.go
@@ -288,10 +288,10 @@ func (s *Schemer) IsHostInCluster(ctx context.Context, host *chop.ChiHost) bool 
 	//opts := clickhouse.NewQueryOptions()
 	err := s.ExecHost(ctx, host, SQLs, opts)
 	if err == nil {
-		log.V(1).M(host).F().Info("The host is inside the cluster")
+		log.V(1).M(host).F().Info("The host %s is inside the cluster", host.GetName())
 		inside = true
 	} else {
-		log.V(1).M(host).F().Info("The host is outside of the cluster")
+		log.V(1).M(host).F().Info("The host %s is outside of the cluster", host.GetName())
 		inside = false
 	}
 	return inside
