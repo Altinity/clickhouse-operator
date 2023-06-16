c85d9618a40cfd5413653aaa0c178725ca2718a2
Vladislav Klimenko <sunsingerus@gmail.com>
2023-06-16 15:19:52 +0300

    dev: rework watched types

--- pkg/apis/metrics/type_watched_chi.go
+++ pkg/apis/metrics/type_watched_chi.go
@@ -14,11 +14,51 @@
 
 package metrics
 
+import (
+	"encoding/json"
+	v1 "github.com/altinity/clickhouse-operator/pkg/apis/clickhouse.altinity.com/v1"
+)
+
 // WatchedCHI specifies watched CLickHouseInstallation
 type WatchedCHI struct {
-	Namespace string   `json:"namespace"`
-	Name      string   `json:"name"`
-	Hostnames []string `json:"hostnames"`
+	Namespace string            `json:"namespace"`
+	Name      string            `json:"name"`
+	Clusters  []*WatchedCluster `json:"clusters"`
+}
+
+type WatchedCluster struct {
+	Name  string         `json:"name,omitempty"  yaml:"name,omitempty"`
+	Hosts []*WatchedHost `json:"hosts,omitempty" yaml:"hosts,omitempty"`
+}
+
+type WatchedHost struct {
+	Name      string `json:"name,omitempty"      yaml:"name,omitempty"`
+	Hostname  string `json:"hostname,omitempty"  yaml:"hostname,omitempty"`
+	TCPPort   int32  `json:"tcpPort,omitempty"   yaml:"tcpPort,omitempty"`
+	TLSPort   int32  `json:"tlsPort,omitempty"   yaml:"tlsPort,omitempty"`
+	HTTPPort  int32  `json:"httpPort,omitempty"  yaml:"httpPort,omitempty"`
+	HTTPSPort int32  `json:"httpsPort,omitempty" yaml:"httpsPort,omitempty"`
+}
+
+func NewWatchedCHI(c *v1.ClickHouseInstallation) *WatchedCHI {
+	chi := &WatchedCHI{}
+	chi.readFrom(c)
+	return chi
+}
+
+func (chi *WatchedCHI) readFrom(c *v1.ClickHouseInstallation) {
+	if chi == nil {
+		return
+	}
+	chi.Namespace = c.Namespace
+	chi.Name = c.Name
+
+	c.WalkClusters(func(cl *v1.Cluster) error {
+		cluster := &WatchedCluster{}
+		cluster.readFrom(cl)
+		chi.Clusters = append(chi.Clusters, cluster)
+		return nil
+	})
 }
 
 func (chi *WatchedCHI) isValid() bool {
@@ -26,36 +66,54 @@ func (chi *WatchedCHI) isValid() bool {
 }
 
 func (chi *WatchedCHI) empty() bool {
-	return (len(chi.Namespace) == 0) && (len(chi.Name) == 0) && (len(chi.Hostnames) == 0)
-}
-
-func (chi *WatchedCHI) equal(chi2 *WatchedCHI) bool {
-	// Must have the same namespace
-	if chi.Namespace != chi2.Namespace {
-		return false
-	}
-
-	// Must have the same name
-	if chi.Name != chi2.Name {
-		return false
-	}
-
-	// Must have the same number of items
-	if len(chi.Hostnames) != len(chi2.Hostnames) {
-		return false
-	}
-
-	// Must have the same items
-	for i := range chi.Hostnames {
-		if chi.Hostnames[i] != chi2.Hostnames[i] {
-			return false
-		}
-	}
-
-	// All checks passed
-	return true
+	return (len(chi.Namespace) == 0) && (len(chi.Name) == 0) && (len(chi.Clusters) == 0)
 }
 
 func (chi *WatchedCHI) indexKey() string {
 	return chi.Namespace + ":" + chi.Name
+}
+
+func (chi *WatchedCHI) walkHosts(f func(*WatchedCHI, *WatchedCluster, *WatchedHost)) {
+	if chi == nil {
+		return
+	}
+	for _, cluster := range chi.Clusters {
+		for _, host := range cluster.Hosts {
+			f(chi, cluster, host)
+		}
+	}
+}
+
+func (chi *WatchedCHI) String() string {
+	if chi == nil {
+		return "nil"
+	}
+	bytes, _ := json.Marshal(chi)
+	return string(bytes)
+}
+
+func (cluster *WatchedCluster) readFrom(c *v1.Cluster) {
+	if cluster == nil {
+		return
+	}
+	cluster.Name = c.Name
+
+	c.WalkHosts(func(h *v1.ChiHost) error {
+		host := &WatchedHost{}
+		host.readFrom(h)
+		cluster.Hosts = append(cluster.Hosts, host)
+		return nil
+	})
+}
+
+func (host *WatchedHost) readFrom(h *v1.ChiHost) {
+	if host == nil {
+		return
+	}
+	host.Name = h.Name
+	host.Hostname = h.Address.FQDN
+	host.TCPPort = h.TCPPort
+	host.TLSPort = h.TLSPort
+	host.HTTPPort = h.HTTPPort
+	host.HTTPSPort = h.HTTPSPort
 }
