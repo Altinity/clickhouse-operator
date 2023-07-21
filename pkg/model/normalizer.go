cc92c1dd018892591a7cd0a70fb86a9d20909065
Vladislav Klimenko <sunsingerus@gmail.com>
2023-07-21 17:26:32 +0300

    dev: use chi selector

--- pkg/model/normalizer.go
+++ pkg/model/normalizer.go
@@ -122,9 +122,14 @@ func (n *Normalizer) CreateTemplatedCHI(
 	// Apply CHOP-specified templates
 	// TODO
 
-	// Apply CHI-specified templates
+	//
+	// Apply templates both auto-templates as well as explicitly requested
+	//
+
+	// useTemplates specifies list of templated to be applied to the CHI
 	var useTemplates []chiV1.ChiUseTemplate
-	// 1. Get list of auto templates to be applied
+
+	// 1. Get list of auto templates listed in the system
 	if autoTemplates := chop.Config().GetAutoTemplates(); len(autoTemplates) > 0 {
 		log.V(1).M(chi).F().Info("Found auto-templates num: %d", len(autoTemplates))
 		for _, template := range autoTemplates {
@@ -136,7 +141,7 @@ func (n *Normalizer) CreateTemplatedCHI(
 			})
 		}
 	}
-	// 2. Append templates, explicitly requested in CHI to the list of templates to be applied
+	// 2. Append templates, explicitly requested in CHI
 	if len(chi.Spec.UseTemplates) > 0 {
 		useTemplates = append(useTemplates, chi.Spec.UseTemplates...)
 	}
@@ -146,31 +151,42 @@ func (n *Normalizer) CreateTemplatedCHI(
 		n.normalizeUseTemplates(useTemplates)
 	}
 
-	// Apply templates (auto and explicitly requested)
+	// Apply templates (both auto and explicitly requested)
 	for i := range useTemplates {
 		useTemplate := &useTemplates[i]
-		if template := chop.Config().FindTemplate(useTemplate, chi.Namespace); template == nil {
-			log.V(1).M(chi).F().Warning("UNABLE to find template referenced in useTemplates: %s/%s", useTemplate.Namespace, useTemplate.Name)
-		} else {
-			// Apply template
-			(&n.ctx.chi.Spec).MergeFrom(&template.Spec, chiV1.MergeTypeOverrideByNonEmptyValues)
-			n.ctx.chi.Labels = util.MergeStringMapsOverwrite(
-				n.ctx.chi.Labels,
-				util.CopyMapFilter(
-					template.Labels,
-					chop.Config().Label.Include,
-					chop.Config().Label.Exclude,
-				),
-			)
-			n.ctx.chi.Annotations = util.MergeStringMapsOverwrite(
-				n.ctx.chi.Annotations, util.CopyMapFilter(
-					template.Annotations,
-					chop.Config().Annotation.Include,
-					append(chop.Config().Annotation.Exclude, util.ListSkippedAnnotations()...),
-				),
-			)
-			log.V(1).M(chi).F().Info("Merge template referenced in useTemplates: %s/%s", useTemplate.Namespace, useTemplate.Name)
+		template := chop.Config().FindTemplate(useTemplate, chi.Namespace)
+
+		if template == nil {
+			log.V(1).M(chi).F().Warning("Skip template: %s/%s UNABLE to find listed template. ", useTemplate.Namespace, useTemplate.Name)
+			continue // skip to the next template
 		}
+
+		selector := template.Spec.Templating.GetCHISelector()
+		labels := chi.Labels
+
+		if selector.Matches(labels) {
+			log.V(1).M(chi).F().Info("Skip template: %s/%s. Selector %v does not match labels %v", useTemplate.Namespace, useTemplate.Name, selector, labels)
+			continue // skip to the next template
+		}
+
+		// Apply template
+		log.V(1).M(chi).F().Info("Apply template: %s/%s. Selector %v matches labels %v", useTemplate.Namespace, useTemplate.Name, selector, labels)
+		(&n.ctx.chi.Spec).MergeFrom(&template.Spec, chiV1.MergeTypeOverrideByNonEmptyValues)
+		n.ctx.chi.Labels = util.MergeStringMapsOverwrite(
+			n.ctx.chi.Labels,
+			util.CopyMapFilter(
+				template.Labels,
+				chop.Config().Label.Include,
+				chop.Config().Label.Exclude,
+			),
+		)
+		n.ctx.chi.Annotations = util.MergeStringMapsOverwrite(
+			n.ctx.chi.Annotations, util.CopyMapFilter(
+				template.Annotations,
+				chop.Config().Annotation.Include,
+				append(chop.Config().Annotation.Exclude, util.ListSkippedAnnotations()...),
+			),
+		)
 	}
 
 	// After all templates applied, place provided CHI on top of the whole stack
