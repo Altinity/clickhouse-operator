b1b0ebbc6df1861e4e1e2b79b02fa607ecfd254a
Vladislav Klimenko <sunsingerus@gmail.com>
2023-07-24 00:18:19 +0300

    dev: adjust operator hub manifests builder

--- deploy/builder/operatorhub.sh
+++ deploy/builder/operatorhub.sh
@@ -17,8 +17,8 @@ echo "Please, ensure PREVIOUS_VERSION=$PREVIOUS_VERSION is what is needed"
 read -n 1 -r -s -p $'Press enter to continue...\n'
 
 OPERATORHUB_DIR="${SRC_ROOT}/deploy/operatorhub"
-DST_DIR="${OPERATORHUB_DIR}/${VERSION}"
-mkdir -p ${DST_DIR}
+MANIFESTS_DIR="${OPERATORHUB_DIR}/${VERSION}"
+mkdir -p "${MANIFESTS_DIR}"
 
 #yq eval 'select(documentIndex == 0) | .rules' deploy/builder/templates-install-bundle/clickhouse-operator-install-yaml-template-02-section-rbac-02-role.yaml
 #yq eval '.spec.install.spec.permissions[0].rules' deploy/builder/templates-operatorhub/clickhouse-operator.vVERSION.clusterserviceversion.yaml
@@ -27,21 +27,23 @@ mkdir -p ${DST_DIR}
 #yq eval-all 'select(fileIndex==0).a = select(fileIndex==1) | select(fileIndex==0)' sample.yml another.yml
 
 
-CVV_FILE_TEMPLATE="${DST_DIR}/clickhouse-operator.v${VERSION}.clusterserviceversion-template.yaml"
-CVV_FILE="${DST_DIR}/clickhouse-operator.v${VERSION}.clusterserviceversion.yaml"
+CVV_FILE_TEMPLATE="${MANIFESTS_DIR}/clickhouse-operator.v${VERSION}.clusterserviceversion-template.yaml"
+CVV_FILE="${MANIFESTS_DIR}/clickhouse-operator.v${VERSION}.clusterserviceversion.yaml"
 
 cp -f \
   "${SRC_ROOT}/deploy/builder/templates-operatorhub/clickhouse-operator.vVERSION.clusterserviceversion-template.yaml" \
   "${CVV_FILE_TEMPLATE}"
 
-RBAC_FILE="${SRC_ROOT}/deploy/builder/templates-install-bundle/clickhouse-operator-install-yaml-template-02-section-rbac-02-role.yaml"
+RBAC_ROLE_FILE="${SRC_ROOT}/deploy/builder/templates-install-bundle/clickhouse-operator-install-yaml-template-02-section-rbac-02-role.yaml"
 
 #yq 'select(documentIndex == 0).rules' "${RBAC_FILE}" | yq 'select(fileIndex==0).spec.install.spec.permissions[0].rules = select(fileIndex==1)' "${CVV_FILE}" -
 #yq eval --inplace 'select(documentIndex==0)' "${CVV_FILE}"
 
-TMP="qwe.yaml"
-yq 'select(documentIndex == 0).rules' "${RBAC_FILE}" > $TMP
-yq -i ".spec.install.spec.permissions[0].rules = load(\"$TMP\")" "${CVV_FILE_TEMPLATE}"
+# Extract rules from the role file and insert rules into CVV file
+RBAC_ROLE_RULES_FILE="qwe.yaml"
+yq 'select(documentIndex == 0).rules' "${RBAC_ROLE_FILE}" > "${RBAC_ROLE_RULES_FILE}"
+yq -i ".spec.install.spec.permissions[0].rules = load(\"${RBAC_ROLE_RULES_FILE}\")" "${CVV_FILE_TEMPLATE}"
+rm "${RBAC_ROLE_RULES_FILE}"
 
 # Build partial .yaml manifest(s)
 #MANIFEST_PRINT_CRD="no" \
@@ -56,23 +58,21 @@ yq -i ".spec.install.spec.permissions[0].rules = load(\"$TMP\")" "${CVV_FILE_TEM
 #yq eval -o=json "${SRC_ROOT}/docs/chi-examples/50-chi-template-01.yaml"
 #yq eval -o=json "${SRC_ROOT}/docs/chi-examples/70-chop-config.yaml"
 
-TMP="qwe.yaml"
+# Insert examples into CVV file
+EXAMPLES_FILE="qwe.yaml"
 F1="${SRC_ROOT}/docs/chi-examples/01-simple-layout-01-1shard-1repl.yaml"
 F2="${SRC_ROOT}/docs/chi-examples/50-chi-template-01.yaml"
 F3="${SRC_ROOT}/docs/chi-examples/70-chop-config.yaml"
-yq -n -I=2 -o=json ".[0] = load(\"${F1}\") | .[1] = load(\"${F2}\") | .[2] = load(\"${F3}\")" > $TMP
-yq -i ".metadata.annotations.alm-examples = strload(\"$TMP\")" "${CVV_FILE_TEMPLATE}"
-rm $TMP
+yq -n -I=2 -o=json ".[0] = load(\"${F1}\") | .[1] = load(\"${F2}\") | .[2] = load(\"${F3}\")" > "${EXAMPLES_FILE}"
+yq -i ".metadata.annotations.alm-examples = strload(\"${EXAMPLES_FILE}\")" "${CVV_FILE_TEMPLATE}"
+rm "${EXAMPLES_FILE}"
 
 cat "${CVV_FILE_TEMPLATE}" | \
     OPERATOR_VERSION="${VERSION}" \
     PREVIOUS_OPERATOR_VERSION="${PREVIOUS_VERSION}" \
-    envsubst > $CVV_FILE
+    envsubst > "${CVV_FILE}"
 
-rm $CVV_FILE_TEMPLATE
-
-
-
+rm "${CVV_FILE_TEMPLATE}"
 
 #$CHIT.crd.yaml
 #$CONF.crd.yaml
@@ -88,7 +88,7 @@ MANIFEST_PRINT_RBAC_CLUSTERED="no" \
 MANIFEST_PRINT_RBAC_NAMESPACED="no" \
 MANIFEST_PRINT_DEPLOYMENT="no" \
 MANIFEST_PRINT_SERVICE_METRICS="no" \
-"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CHI}\")" > "${DST_DIR}/${CHI}.crd.yaml"
+"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CHI}\")" > "${MANIFESTS_DIR}/${CHI}.crd.yaml"
 
 # Build partial .yaml manifest(s)
 MANIFEST_PRINT_CRD="yes" \
@@ -96,7 +96,7 @@ MANIFEST_PRINT_RBAC_CLUSTERED="no" \
 MANIFEST_PRINT_RBAC_NAMESPACED="no" \
 MANIFEST_PRINT_DEPLOYMENT="no" \
 MANIFEST_PRINT_SERVICE_METRICS="no" \
-"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CHIT}\")" > "${DST_DIR}/${CHIT}.crd.yaml"
+"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CHIT}\")" > "${MANIFESTS_DIR}/${CHIT}.crd.yaml"
 
 # Build partial .yaml manifest(s)
 MANIFEST_PRINT_CRD="yes" \
@@ -104,13 +104,14 @@ MANIFEST_PRINT_RBAC_CLUSTERED="no" \
 MANIFEST_PRINT_RBAC_NAMESPACED="no" \
 MANIFEST_PRINT_DEPLOYMENT="no" \
 MANIFEST_PRINT_SERVICE_METRICS="no" \
-"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CONF}\")" > "${DST_DIR}/${CONF}.crd.yaml"
+"${CUR_DIR}/cat-clickhouse-operator-install-yaml.sh" | yq "select(.metadata.name == \"${CONF}\")" > "${MANIFESTS_DIR}/${CONF}.crd.yaml"
 
-
-cat <<EOF > "${OPERATORHUB_DIR}/clickhouse.package.yaml"
-channels:
-- currentCSV: clickhouse-operator.v${VERSION}
-  name: latest
-defaultChannel: latest
-packageName: clickhouse
-EOF
+# TODO
+# Package file not used any more?
+#cat <<EOF > "${OPERATORHUB_DIR}/clickhouse.package.yaml"
+#channels:
+#- currentCSV: clickhouse-operator.v${VERSION}
+#  name: latest
+#defaultChannel: latest
+#packageName: clickhouse
+#EOF
