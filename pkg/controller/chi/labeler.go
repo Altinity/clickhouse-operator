4086d8116e7b01163ae59f8120c64181ac112def
Vladislav Klimenko <sunsingerus@gmail.com>
2023-09-18 10:23:38 +0300

    dev: rename labeler functions to relay real behavior

--- pkg/controller/chi/labeler.go
+++ pkg/controller/chi/labeler.go
@@ -246,8 +246,8 @@ func (c *Controller) addLabels(labels map[string]string) map[string]string {
 	)
 }
 
-// appendLabelReadyPod appends Label "Ready" to the pod of the specified host
-func (c *Controller) appendLabelReadyPod(ctx context.Context, host *chiV1.ChiHost) error {
+// appendLabelReadyOnPod appends Label "Ready" to the pod of the specified host
+func (c *Controller) appendLabelReadyOnPod(ctx context.Context, host *chiV1.ChiHost) error {
 	if util.IsContextDone(ctx) {
 		log.V(2).Info("task is done")
 		return nil
@@ -302,8 +302,8 @@ func (c *Controller) deleteLabelReadyPod(ctx context.Context, host *chiV1.ChiHos
 	return nil
 }
 
-// appendAnnotationReadyService appends Annotation "Ready" to the service of the specified host
-func (c *Controller) appendAnnotationReadyService(ctx context.Context, host *chiV1.ChiHost) error {
+// appendAnnotationReadyOnService appends Annotation "Ready" to the service of the specified host
+func (c *Controller) appendAnnotationReadyOnService(ctx context.Context, host *chiV1.ChiHost) error {
 	if util.IsContextDone(ctx) {
 		log.V(2).Info("task is done")
 		return nil
