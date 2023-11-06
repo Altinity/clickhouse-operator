24772be4672845622f90c026e64180476f253e3a
Vladislav Klimenko <sunsingerus@gmail.com>
2023-11-06 13:38:30 +0300

    dev: add uniqueness check for prepared list of templates

--- pkg/apis/clickhouse.altinity.com/v1/type_config_chop.go
+++ pkg/apis/clickhouse.altinity.com/v1/type_config_chop.go
@@ -516,18 +516,11 @@ func (c *OperatorConfig) readCHITemplates() (errs []error) {
 
 // enlistCHITemplate inserts template into templates catalog
 func (c *OperatorConfig) enlistCHITemplate(template *ClickHouseInstallation) {
-	if c.Template.CHI.Runtime.Templates == nil {
-		c.Template.CHI.Runtime.Templates = make([]*ClickHouseInstallation, 0)
-	}
 	c.Template.CHI.Runtime.Templates = append(c.Template.CHI.Runtime.Templates, template)
 }
 
 // unlistCHITemplate removes template from templates catalog
 func (c *OperatorConfig) unlistCHITemplate(template *ClickHouseInstallation) {
-	if c.Template.CHI.Runtime.Templates == nil {
-		return
-	}
-
 	// Nullify found template entry
 	for _, _template := range c.Template.CHI.Runtime.Templates {
 		if (_template.Name == template.Name) && (_template.Namespace == template.Namespace) {
@@ -541,8 +534,8 @@ func (c *OperatorConfig) unlistCHITemplate(template *ClickHouseInstallation) {
 	// TODO compact the slice
 }
 
-// FindTemplate finds specified template
-func (c *OperatorConfig) FindTemplate(use *ChiUseTemplate, namespace string) *ClickHouseInstallation {
+// FindTemplate finds specified template within possibly specified namespace
+func (c *OperatorConfig) FindTemplate(use *ChiUseTemplate, fallbackNamespace string) *ClickHouseInstallation {
 	// Try to find direct match
 	for _, _template := range c.Template.CHI.Runtime.Templates {
 		if _template.MatchFullName(use.Namespace, use.Name) {
@@ -552,17 +545,18 @@ func (c *OperatorConfig) FindTemplate(use *ChiUseTemplate, namespace string) *Cl
 	}
 
 	// Direct match is not possible.
+	// Let's try to find by name only
 
 	if use.Namespace != "" {
-		// With fully-specified use template direct (full name) only match is applicable, and it is not possible
+		// With fully-specified template namespace+name pair direct (full name) only match is applicable
 		// This is strange situation, however
 		return nil
 	}
 
-	// Improvise with use.Namespace
+	// Look for templates with specified name in explicitly specified namespace
 
 	for _, _template := range c.Template.CHI.Runtime.Templates {
-		if _template.MatchFullName(namespace, use.Name) {
+		if _template.MatchFullName(fallbackNamespace, use.Name) {
 			// Found template with searched name in specified namespace
 			return _template
 		}
@@ -582,38 +576,34 @@ func (c *OperatorConfig) GetAutoTemplates() []*ClickHouseInstallation {
 		}
 	}
 
-	// Prepare sorted list of namespaces
+	// Prepare sorted unique list of namespaces
 	var namespaces []string
 	for _, _template := range autoTemplates {
-		found := false
-		for _, namespace := range namespaces {
-			if namespace == _template.Namespace {
-				// Already has it
-				found = true
-				break
-			}
-		}
-		if !found {
+		// Append template's namespace to the list of namespaces
+		if !util.StringSliceContains(namespaces, _template.Namespace) {
 			namespaces = append(namespaces, _template.Namespace)
 		}
 	}
 	sort.Strings(namespaces)
 
+	// Prepare sorted list of templates
 	var sortedTemplates []*ClickHouseInstallation
+	// Walk over sorted unique namespaces
 	for _, namespace := range namespaces {
-		// Prepare sorted list of names within this namespace
+		// Prepare sorted unique list of names within this namespace
 		var names []string
 		for _, _template := range autoTemplates {
-			if _template.Namespace == namespace {
+			if _template.MatchNamespace(namespace) && !util.StringSliceContains(names, _template.Name) {
 				names = append(names, _template.Name)
 			}
 		}
 		sort.Strings(names)
 
-		// Walk over sorted list of names within this namespace and append to the result list of templates
+		// Walk over sorted unique list of names within this namespace
+		// and append first unseen before template to the result list of templates
 		for _, name := range names {
 			for _, _template := range autoTemplates {
-				if (_template.Namespace == namespace) && (_template.Name == name) {
+				if _template.MatchFullName(namespace, name) && !_template.FoundIn(sortedTemplates) {
 					sortedTemplates = append(sortedTemplates, _template)
 				}
 			}
