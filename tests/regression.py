decec067275a89a9e4256b4ef73bcde98795bac4
Vladislav Klimenko <sunsingerus@gmail.com>
2023-11-29 11:55:43 +0300

    test: do not allow test 34 to fail

--- tests/regression.py
+++ tests/regression.py
@@ -8,7 +8,6 @@ from requirements.requirements import *
 xfails = {
     # test_operator.py
     # "/regression/e2e.test_operator/test_028*": [(Fail, "In case of: 1) operator restarted on the different IP and 2) long time before operator received event, this test would fail due to RollingUpdate option")],
-    "/regression/e2e.test_operator/test_034*": [(Fail, "skip for now")],
     # test_clickhouse.py
     "/regression/e2e.test_clickhouse/test_ch_001*": [(Fail, "Insert Quorum test need to refactoring")],
     # test_metrics_alerts.py
