99a208dd9fc6945dfd823b8bc6a643715b7775dd
Vladislav Klimenko <sunsingerus@gmail.com>
2024-02-07 01:13:35 +0300

    test: add OLM manifests

--- tests/e2e/install_olm.sh
+++ tests/e2e/install_olm.sh
@@ -2,3 +2,6 @@
 
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/upstream/quickstart/crds.yaml
 kubectl create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/upstream/quickstart/olm.yaml
+
+kubectl create ns mynamespace
+kubectl apply -f olm.yaml
