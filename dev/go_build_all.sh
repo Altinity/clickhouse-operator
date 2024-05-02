ac720350dd496bb2b730f54690771726f4c94609
Vladislav Klimenko <sunsingerus@gmail.com>
2024-05-02 12:17:29 +0300

    dev: low verbosity

--- dev/go_build_all.sh
+++ dev/go_build_all.sh
@@ -16,10 +16,10 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "Build helm charts"
 source "${CUR_DIR}/generate_helm_chart.sh"
 
-CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo "Run source checker"
-source "${CUR_DIR}/run_gocard.sh"
-
-CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo "Run security checker"
-source "${CUR_DIR}/run_gosec.sh"
+#CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+#echo "Run source checker"
+#source "${CUR_DIR}/run_gocard.sh"
+#
+#CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+#echo "Run security checker"
+#source "${CUR_DIR}/run_gosec.sh"
