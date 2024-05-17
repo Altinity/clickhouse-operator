ac85f817e4b59a2503be20aa75a5c74fcc617d0d
Vladislav Klimenko <sunsingerus@gmail.com>
2024-05-18 00:51:16 +0300

    dev: extract common k8s volume funcs

---
+++ pkg/model/k8s/volume.go
@@ -0,0 +1,54 @@
+// Copyright 2019 Altinity Ltd and/or its affiliates. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import core "k8s.io/api/core/v1"
+
+// CreateVolumeForPVC returns core.Volume object with specified name
+func CreateVolumeForPVC(name, claimName string) core.Volume {
+	return core.Volume{
+		Name: name,
+		VolumeSource: core.VolumeSource{
+			PersistentVolumeClaim: &core.PersistentVolumeClaimVolumeSource{
+				ClaimName: claimName,
+				ReadOnly:  false,
+			},
+		},
+	}
+}
+
+// CreateVolumeForConfigMap returns core.Volume object with defined name
+func CreateVolumeForConfigMap(name string) core.Volume {
+	var defaultMode int32 = 0644
+	return core.Volume{
+		Name: name,
+		VolumeSource: core.VolumeSource{
+			ConfigMap: &core.ConfigMapVolumeSource{
+				LocalObjectReference: core.LocalObjectReference{
+					Name: name,
+				},
+				DefaultMode: &defaultMode,
+			},
+		},
+	}
+}
+
+// CreateVolumeMount returns core.VolumeMount object with name and mount path
+func CreateVolumeMount(name, mountPath string) core.VolumeMount {
+	return core.VolumeMount{
+		Name:      name,
+		MountPath: mountPath,
+	}
+}
