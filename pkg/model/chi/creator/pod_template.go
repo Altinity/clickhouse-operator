2aa41b8b85e9a2be21dfb9abb2128ac1db4ad733
Vladislav Klimenko <sunsingerus@gmail.com>
2024-05-18 00:55:13 +0300

    dev: extract pod template functions

--- pkg/model/chi/creator/pod_template.go
+++ pkg/model/chi/creator/pod_template.go
@@ -15,6 +15,7 @@
 package creator
 
 import (
+	model "github.com/altinity/clickhouse-operator/pkg/model/chi"
 	core "k8s.io/api/core/v1"
 
 	api "github.com/altinity/clickhouse-operator/pkg/apis/clickhouse.altinity.com/v1"
@@ -22,13 +23,31 @@ import (
 	"github.com/altinity/clickhouse-operator/pkg/model/k8s"
 )
 
-// newDefaultPodTemplate is a unification wrapper
-func newDefaultPodTemplate(host *api.Host) *api.PodTemplate {
-	return newDefaultClickHousePodTemplate(host)
+// getPodTemplate gets Pod Template to be used to create StatefulSet
+func (c *Creator) getPodTemplate(host *api.Host) *api.PodTemplate {
+	// Which pod template should be used - either explicitly defined or a default one
+	podTemplate, ok := host.GetPodTemplate()
+	if ok {
+		// Host references known PodTemplate
+		// Make local copy of this PodTemplate, in order not to spoil the original common-used template
+		podTemplate = podTemplate.DeepCopy()
+		c.a.V(3).F().Info("host: %s StatefulSet - use custom template: %s", host.Runtime.Address.HostName, podTemplate.Name)
+	} else {
+		// Host references UNKNOWN PodTemplate, will use default one
+		podTemplate = c.newAppPodTemplateDefault(host)
+		c.a.V(3).F().Info("host: %s StatefulSet - use default generated template", host.Runtime.Address.HostName)
+	}
+
+	// Here we have local copy of Pod Template, to be used to create StatefulSet
+	// Now we can customize this Pod Template for particular host
+
+	model.PrepareAffinity(podTemplate, host)
+
+	return podTemplate
 }
 
-// newDefaultClickHousePodTemplate returns default Pod Template to be used with StatefulSet
-func newDefaultClickHousePodTemplate(host *api.Host) *api.PodTemplate {
+// newAppPodTemplateDefault is a unification wrapper
+func (c *Creator) newAppPodTemplateDefault(host *api.Host) *api.PodTemplate {
 	podTemplate := &api.PodTemplate{
 		Name: namer.Name(namer.NameStatefulSet, host),
 		Spec: core.PodSpec{
@@ -37,8 +56,8 @@ func newDefaultClickHousePodTemplate(host *api.Host) *api.PodTemplate {
 		},
 	}
 
-	// Pod has to have main container.
-	k8s.PodSpecAddContainer(&podTemplate.Spec, newDefaultClickHouseContainer(host))
+	// Pod has to have application container.
+	k8s.PodSpecAddContainer(&podTemplate.Spec, c.cm.NewDefaultAppContainer(host))
 
 	return podTemplate
 }
