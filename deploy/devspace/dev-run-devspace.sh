581f396b1dbcbdbbb5290d29f1d551540c3c759d
Vladislav Klimenko <sunsingerus@gmail.com>
2024-07-08 14:30:28 +0300

    make runnable

--- deploy/devspace/dev-run-devspace.sh
+++ deploy/devspace/dev-run-devspace.sh
@@ -18,9 +18,9 @@ echo "METRICS_EXPORTER_IMAGE=${METRICS_EXPORTER_IMAGE}"
 echo "DEPLOY_OPERATOR=${DEPLOY_OPERATOR}"
 echo "MINIKUBE=${MINIKUBE}"
 
-export MINIKUBE=${MINIKUBE:-yes}
+export MINIKUBE="${MINIKUBE:-"yes"}"
 
 #
 # Run devspace
 #
-devspace dev --var=OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-kube-system}" --var=DEVSPACE_DEBUG=delve
+devspace dev --var=OPERATOR_NAMESPACE="${OPERATOR_NAMESPACE:-"kube-system"}" --var=DEVSPACE_DEBUG="delve"
