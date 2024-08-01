0d692b88f1fbd70aa9d48120da0731c5d5eca174
Vladislav Klimenko <sunsingerus@gmail.com>
2024-08-01 14:40:12 +0300

    dev: split sts controller

--- pkg/controller/common/statefulset/statefulset-reconciler.go
+++ pkg/controller/common/statefulset/statefulset-reconciler.go
@@ -18,8 +18,6 @@ import (
 	"context"
 	"time"
 
-	"gopkg.in/d4l3k/messagediff.v1"
-
 	apps "k8s.io/api/apps/v1"
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 
@@ -31,29 +29,6 @@ import (
 	"github.com/altinity/clickhouse-operator/pkg/model/k8s"
 	"github.com/altinity/clickhouse-operator/pkg/util"
 )
-
-type IHostStatefulSetPoller interface {
-	WaitHostStatefulSetReady(ctx context.Context, host *api.Host) error
-	WaitHostStatefulSetDeleted(host *api.Host)
-}
-
-type fallback interface {
-	OnStatefulSetCreateFailed(ctx context.Context, host *api.Host) common.ErrorCRUD
-	OnStatefulSetUpdateFailed(ctx context.Context, oldStatefulSet *apps.StatefulSet, host *api.Host, sts interfaces.IKubeSTS) common.ErrorCRUD
-}
-
-type DefaultFallback struct{}
-
-func NewDefaultFallback() *DefaultFallback {
-	return &DefaultFallback{}
-}
-
-func (f *DefaultFallback) OnStatefulSetCreateFailed(ctx context.Context, host *api.Host) common.ErrorCRUD {
-	return common.ErrCRUDIgnore
-}
-func (f *DefaultFallback) OnStatefulSetUpdateFailed(ctx context.Context, oldStatefulSet *apps.StatefulSet, host *api.Host, sts interfaces.IKubeSTS) common.ErrorCRUD {
-	return common.ErrCRUDIgnore
-}
 
 type StatefulSetReconciler struct {
 	a    common.Announcer
@@ -453,33 +428,6 @@ func (r *StatefulSetReconciler) doUpdateStatefulSet(
 
 	log.V(2).M(host).F().Info("Target generation reached, StatefulSet updated successfully")
 	return nil
-}
-
-func dumpDiff(old, new *apps.StatefulSet) string {
-	diff, equal := messagediff.DeepDiff(old.Spec, new.Spec)
-
-	str := ""
-	if equal {
-		str += "EQUAL: "
-	} else {
-		str += "NOT EQUAL: "
-	}
-
-	if len(diff.Added) > 0 {
-		// Something added
-		str += util.MessageDiffItemString("added spec items", "none", "", diff.Added)
-	}
-
-	if len(diff.Removed) > 0 {
-		// Something removed
-		str += util.MessageDiffItemString("removed spec items", "none", "", diff.Removed)
-	}
-
-	if len(diff.Modified) > 0 {
-		// Something modified
-		str += util.MessageDiffItemString("modified spec items", "none", "", diff.Modified)
-	}
-	return str
 }
 
 // deleteStatefulSet gracefully deletes StatefulSet through zeroing Pod's count
