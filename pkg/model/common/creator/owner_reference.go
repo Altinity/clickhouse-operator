72258750352b3de3a4947be45028abd65a3ebf3e
Vladislav Klimenko <sunsingerus@gmail.com>
2024-08-08 12:57:16 +0300

    dev: generalize owner referenceer

--- pkg/model/common/creator/owner_reference.go
+++ pkg/model/common/creator/owner_reference.go
@@ -14,28 +14,39 @@
 package creator
 
 import (
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	api "github.com/altinity/clickhouse-operator/pkg/apis/clickhouse.altinity.com/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type OwnerReferencer struct {
+	APIVersion string
+	Kind       string
+}
+
+func NewOwnerReferencer(apiVersion, kind string) *OwnerReferencer {
+	return &OwnerReferencer{
+		APIVersion: apiVersion,
+		Kind:       kind,
+	}
+}
+
 // CreateOwnerReferences gets MULTIPLE owner references
-func CreateOwnerReferences(owner api.ICustomResource) []meta.OwnerReference {
+func (r *OwnerReferencer) CreateOwnerReferences(owner api.ICustomResource) []meta.OwnerReference {
 	if owner.GetRuntime().GetAttributes().GetSkipOwnerRef() {
 		return nil
 	}
 	return []meta.OwnerReference{
-		createOwnerReference(owner),
+		r.createOwnerReference(owner),
 	}
 }
 
 // createOwnerReference gets ONE owner reference
-func createOwnerReference(m meta.Object) meta.OwnerReference {
+func (r *OwnerReferencer) createOwnerReference(m meta.Object) meta.OwnerReference {
 	controller := true
 	block := true
 	return meta.OwnerReference{
-		APIVersion:         api.SchemeGroupVersion.String(),
-		Kind:               api.ClickHouseInstallationCRDResourceKind,
+		APIVersion:         r.APIVersion,
+		Kind:               r.Kind,
 		Name:               m.GetName(),
 		UID:                m.GetUID(),
 		Controller:         &controller,
