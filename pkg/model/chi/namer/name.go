e85d222b0b4254ef36920dd5eb0e38696b4a47a7
Vladislav Klimenko <sunsingerus@gmail.com>
2024-08-14 11:05:36 +0300

    dev: introduce chi namer

--- pkg/model/chi/namer/name.go
+++ pkg/model/chi/namer/name.go
@@ -15,21 +15,285 @@
 package namer
 
 import (
+	"fmt"
+
+	apps "k8s.io/api/apps/v1"
+
 	api "github.com/altinity/clickhouse-operator/pkg/apis/clickhouse.altinity.com/v1"
-	"github.com/altinity/clickhouse-operator/pkg/model/common/namer/macro"
+	"github.com/altinity/clickhouse-operator/pkg/apis/common/types"
+	"github.com/altinity/clickhouse-operator/pkg/util"
 )
 
 // createConfigMapNameCommon returns a name for a ConfigMap for replica's common config
-func createConfigMapNameCommon(chi api.ICustomResource) string {
-	return macro.Macro(chi).Line(configMapNamePatternCommon)
+func (n *Namer) createConfigMapNameCommon(chi api.ICustomResource) string {
+	return n.macro.Scope(chi).Line(patterns.Get(namePatternConfigMapCommon))
 }
 
 // createConfigMapNameCommonUsers returns a name for a ConfigMap for replica's common users config
-func createConfigMapNameCommonUsers(chi api.ICustomResource) string {
-	return macro.Macro(chi).Line(configMapNamePatternCommonUsers)
+func (n *Namer) createConfigMapNameCommonUsers(chi api.ICustomResource) string {
+	return n.macro.Scope(chi).Line(patterns.Get(namePatternConfigMapCommonUsers))
 }
 
 // createConfigMapNameHost returns a name for a ConfigMap for replica's personal config
-func createConfigMapNameHost(host *api.Host) string {
-	return macro.Macro(host).Line(configMapNamePatternHost)
+func (n *Namer) createConfigMapNameHost(host *api.Host) string {
+	return n.macro.Scope(host).Line(patterns.Get(namePatternConfigMapHost))
+}
+
+// createCRServiceName creates a name of a root ClickHouseInstallation Service resource
+func (n *Namer) createCRServiceName(cr api.ICustomResource) string {
+	// Name can be generated either from default name pattern,
+	// or from personal name pattern provided in ServiceTemplate
+
+	// Start with default name pattern
+	pattern := patterns.Get(namePatternCRService)
+
+	// ServiceTemplate may have personal name pattern specified
+	if template, ok := cr.GetRootServiceTemplate(); ok {
+		// ServiceTemplate available
+		if template.GenerateName != "" {
+			// ServiceTemplate has explicitly specified name pattern
+			pattern = template.GenerateName
+		}
+	}
+
+	// Create Service name based on name pattern available
+	return n.macro.Scope(cr).Line(pattern)
+}
+
+// createCRServiceFQDN creates a FQD name of a root ClickHouseInstallation Service resource
+func (n *Namer) createCRServiceFQDN(cr api.ICustomResource, namespaceDomainPattern *types.String) string {
+	// FQDN can be generated either from default pattern,
+	// or from personal pattern provided
+
+	// Start with default pattern
+	pattern := patternServiceFQDN
+
+	if namespaceDomainPattern.HasValue() {
+		// NamespaceDomainPattern has been explicitly specified
+		pattern = "%s." + namespaceDomainPattern.Value()
+	}
+
+	// Create FQDN based on pattern available
+	return fmt.Sprintf(
+		pattern,
+		n.createCRServiceName(cr),
+		cr.GetNamespace(),
+	)
+}
+
+// createClusterServiceName returns a name of a cluster's Service
+func (n *Namer) createClusterServiceName(cluster api.ICluster) string {
+	// Name can be generated either from default name pattern,
+	// or from personal name pattern provided in ServiceTemplate
+
+	// Start with default name pattern
+	pattern := patterns.Get(namePatternClusterService)
+
+	// ServiceTemplate may have personal name pattern specified
+	if template, ok := cluster.GetServiceTemplate(); ok {
+		// ServiceTemplate available
+		if template.GenerateName != "" {
+			// ServiceTemplate has explicitly specified name pattern
+			pattern = template.GenerateName
+		}
+	}
+
+	// Create Service name based on name pattern available
+	return n.macro.Scope(cluster).Line(pattern)
+}
+
+// createShardServiceName returns a name of a shard's Service
+func (n *Namer) createShardServiceName(shard api.IShard) string {
+	// Name can be generated either from default name pattern,
+	// or from personal name pattern provided in ServiceTemplate
+
+	// Start with default name pattern
+	pattern := patterns.Get(namePatternShardService)
+
+	// ServiceTemplate may have personal name pattern specified
+	if template, ok := shard.GetServiceTemplate(); ok {
+		// ServiceTemplate available
+		if template.GenerateName != "" {
+			// ServiceTemplate has explicitly specified name pattern
+			pattern = template.GenerateName
+		}
+	}
+
+	// Create Service name based on name pattern available
+	return n.macro.Scope(shard).Line(pattern)
+}
+
+// createStatefulSetName creates a name of a StatefulSet for ClickHouse instance
+func (n *Namer) createStatefulSetName(host *api.Host) string {
+	// Name can be generated either from default name pattern,
+	// or from personal name pattern provided in PodTemplate
+
+	// Start with default name pattern
+	pattern := patterns.Get(namePatternStatefulSet)
+
+	// PodTemplate may have personal name pattern specified
+	if template, ok := host.GetPodTemplate(); ok {
+		// PodTemplate available
+		if template.GenerateName != "" {
+			// PodTemplate has explicitly specified name pattern
+			pattern = template.GenerateName
+		}
+	}
+
+	// Create StatefulSet name based on name pattern available
+	return n.macro.Scope(host).Line(pattern)
+}
+
+// createStatefulSetServiceName returns a name of a StatefulSet-related Service for ClickHouse instance
+func (n *Namer) createStatefulSetServiceName(host *api.Host) string {
+	// Name can be generated either from default name pattern,
+	// or from personal name pattern provided in ServiceTemplate
+
+	// Start with default name pattern
+	pattern := patterns.Get(namePatternStatefulSetService)
+
+	// ServiceTemplate may have personal name pattern specified
+	if template, ok := host.GetServiceTemplate(); ok {
+		// ServiceTemplate available
+		if template.GenerateName != "" {
+			// ServiceTemplate has explicitly specified name pattern
+			pattern = template.GenerateName
+		}
+	}
+
+	// Create Service name based on name pattern available
+	return n.macro.Scope(host).Line(pattern)
+}
+
+// createPodHostname returns a hostname of a Pod of a ClickHouse instance.
+// Is supposed to be used where network connection to a Pod is required.
+// NB: right now Pod's hostname points to a Service, through which Pod can be accessed.
+func (n *Namer) createPodHostname(host *api.Host) string {
+	// Do not use Pod own hostname - point to appropriate StatefulSet's Service
+	return n.createStatefulSetServiceName(host)
+}
+
+// createInstanceHostname returns hostname (pod-hostname + service or FQDN) which can be used as a replica name
+// in all places where ClickHouse requires replica name. These are such places as:
+// 1. "remote_servers.xml" config file
+// 2. statements like SYSTEM DROP REPLICA <replica_name>
+// any other places
+// Function operations are based on .Spec.Defaults.ReplicasUseFQDN
+func (n *Namer) createInstanceHostname(host *api.Host) string {
+	if host.GetCR().GetSpec().GetDefaults().ReplicasUseFQDN.IsTrue() {
+		// In case .Spec.Defaults.ReplicasUseFQDN is set replicas would use FQDN pod hostname,
+		// otherwise hostname+service name (unique within namespace) would be used
+		// .my-dev-namespace.svc.cluster.local
+		return n.createPodFQDN(host)
+	}
+
+	return n.createPodHostname(host)
+}
+
+// createPodFQDN creates a fully qualified domain name of a pod
+// ss-1eb454-2-0.my-dev-domain.svc.cluster.local
+func (n *Namer) createPodFQDN(host *api.Host) string {
+	// FQDN can be generated either from default pattern,
+	// or from personal pattern provided
+
+	// Start with default pattern
+	pattern := patternPodFQDN
+
+	if host.GetCR().GetSpec().GetNamespaceDomainPattern().HasValue() {
+		// NamespaceDomainPattern has been explicitly specified
+		pattern = "%s." + host.GetCR().GetSpec().GetNamespaceDomainPattern().Value()
+	}
+
+	// Create FQDN based on pattern available
+	return fmt.Sprintf(
+		pattern,
+		n.createPodHostname(host),
+		host.GetRuntime().GetAddress().GetNamespace(),
+	)
+}
+
+// createPodFQDNsOfCluster creates fully qualified domain names of all pods in a cluster
+func (n *Namer) createPodFQDNsOfCluster(cluster api.ICluster) (fqdns []string) {
+	cluster.WalkHosts(func(host *api.Host) error {
+		fqdns = append(fqdns, n.createPodFQDN(host))
+		return nil
+	})
+	return fqdns
+}
+
+// createPodFQDNsOfShard creates fully qualified domain names of all pods in a shard
+func (n *Namer) createPodFQDNsOfShard(shard api.IShard) (fqdns []string) {
+	shard.WalkHosts(func(host *api.Host) error {
+		fqdns = append(fqdns, n.createPodFQDN(host))
+		return nil
+	})
+	return fqdns
+}
+
+// createPodFQDNsOfCHI creates fully qualified domain names of all pods in a CHI
+func (n *Namer) createPodFQDNsOfCHI(chi api.ICustomResource) (fqdns []string) {
+	chi.WalkHosts(func(host *api.Host) error {
+		fqdns = append(fqdns, n.createPodFQDN(host))
+		return nil
+	})
+	return fqdns
+}
+
+// createFQDN is a wrapper over pod FQDN function
+func (n *Namer) createFQDN(host *api.Host) string {
+	return n.createPodFQDN(host)
+}
+
+// createFQDNs is a wrapper over set of create FQDN functions
+// obj specifies source object to create FQDNs from
+// scope specifies target scope - what entity to create FQDNs for - be it CHI, cluster, shard or a host
+// excludeSelf specifies whether to exclude the host itself from the result. Applicable only in case obj is a host
+func (n *Namer) createFQDNs(obj interface{}, scope interface{}, excludeSelf bool) []string {
+	switch typed := obj.(type) {
+	case api.ICustomResource:
+		return n.createPodFQDNsOfCHI(typed)
+	case api.ICluster:
+		return n.createPodFQDNsOfCluster(typed)
+	case api.IShard:
+		return n.createPodFQDNsOfShard(typed)
+	case *api.Host:
+		self := ""
+		if excludeSelf {
+			self = n.createPodFQDN(typed)
+		}
+		switch scope.(type) {
+		case api.Host:
+			return util.RemoveFromArray(self, []string{n.createPodFQDN(typed)})
+		case api.ChiShard:
+			return util.RemoveFromArray(self, n.createPodFQDNsOfShard(any(typed.GetShard()).(api.IShard)))
+		case api.ChiCluster:
+			return util.RemoveFromArray(self, n.createPodFQDNsOfCluster(any(typed.GetCluster()).(api.ICluster)))
+		case api.ClickHouseInstallation:
+			return util.RemoveFromArray(self, n.createPodFQDNsOfCHI(any(typed.GetCR()).(api.ICustomResource)))
+		}
+	}
+	return nil
+}
+
+// createPodName creates Pod name based on specified StatefulSet or Host
+func (n *Namer) createPodName(obj interface{}) string {
+	switch obj.(type) {
+	case *apps.StatefulSet:
+		statefulSet := obj.(*apps.StatefulSet)
+		return fmt.Sprintf(patternPodName, statefulSet.Name)
+	case *api.Host:
+		host := obj.(*api.Host)
+		return fmt.Sprintf(patternPodName, n.createStatefulSetName(host))
+	}
+	return "unknown-type"
+}
+
+// createPVCName is an internal function
+func (n *Namer) createPVCName(host *api.Host, volumeMountName string) string {
+	return volumeMountName + "-" + n.createPodName(host)
+}
+
+// createPVCNameByVolumeClaimTemplate creates PVC name
+func (n *Namer) createPVCNameByVolumeClaimTemplate(host *api.Host, volumeClaimTemplate *api.VolumeClaimTemplate) string {
+	return n.createPVCName(host, volumeClaimTemplate.Name)
 }
