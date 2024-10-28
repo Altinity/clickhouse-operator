e62296a7be34978124027572392ee13c9bbd09d7
Vladislav Klimenko <sunsingerus@gmail.com>
2024-10-28 18:05:07 +0300

    dedv: extract operator labels

---
+++ pkg/metrics/operator/labels.go
@@ -0,0 +1,61 @@
+// Copyright 2019 Altinity Ltd and/or its affiliates. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operator
+
+import (
+	"github.com/altinity/clickhouse-operator/pkg/util"
+)
+
+func GetLabelsFromSource(src labelsSource) (labels []string, values []string) {
+	labelsFromNames, valuesFromNames := getLabelsFromName(src)
+	labels = append(labels, labelsFromNames...)
+	values = append(values, valuesFromNames...)
+
+	labelsFromLabels, valuesFromLabels := getLabelsFromLabels(src)
+	labels = append(labels, labelsFromLabels...)
+	values = append(values, valuesFromLabels...)
+
+	labelsFromAnnotations, valuesFromAnnotations := getLabelsFromAnnotations(src)
+	labels = append(labels, labelsFromAnnotations...)
+	values = append(values, valuesFromAnnotations...)
+
+	return labels, values
+}
+
+type labelsSource interface {
+	GetName() string
+	GetNamespace() string
+	GetLabels() map[string]string
+	GetAnnotations() map[string]string
+}
+
+func getLabelsFromName(chi labelsSource) (labels []string, values []string) {
+	return []string{"chi", "namespace"}, []string{chi.GetName(), chi.GetNamespace()}
+}
+
+func getLabelsFromLabels(chi labelsSource) (labels []string, values []string) {
+	return util.MapGetSortedKeysAndValues(chi.GetLabels())
+}
+
+func getLabelsFromAnnotations(chi labelsSource) (labels []string, values []string) {
+	return util.MapGetSortedKeysAndValues(
+		// Exclude skipped annotations
+		util.CopyMapFilter(
+			chi.GetAnnotations(),
+			nil,
+			util.ListSkippedAnnotations(),
+		),
+	)
+}
