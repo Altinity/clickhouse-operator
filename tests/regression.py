f37b75c90161178d379fcfb3e07de815e4be9351
Vladislav Klimenko <sunsingerus@gmail.com>
2024-12-14 01:26:08 +0300

    test: allow 47 to flip

--- tests/regression.py
+++ tests/regression.py
@@ -11,6 +11,7 @@ xfails = {
     "/regression/e2e.test_operator/test_014*": [(Fail, "Test 014 sometimes fails due to unknown reasons")],
     "/regression/e2e.test_operator/test_032*": [(Fail, "Test 032 sometimes fails due to unknown reasons")],
     "/regression/e2e.test_operator/test_034*": [(Fail, "Test 034 is incorrect - metrics errors require reconcile")],
+    "/regression/e2e.test_operator/test_047*": [(Fail, "Test 047 sometimes fails due to unknown reasons")],
     # test_clickhouse.py
     "/regression/e2e.test_clickhouse/test_ch_001*": [(Fail, "Insert Quorum test need to refactoring")],
     # test_metrics_alerts.py
