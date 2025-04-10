11286cf0d5b4e6db795f7cd7b402dd9fb1358b83
Vladislav Klimenko <sunsingerus@gmail.com>
2025-04-10 16:20:02 +0300

    dev: streamline chi normalizer

--- pkg/model/chi/normalizer/normalizer.go
+++ pkg/model/chi/normalizer/normalizer.go
@@ -82,20 +82,24 @@ func (n *Normalizer) buildRequest(options *normalizer.Options) {
 
 func (n *Normalizer) buildTargetFromTemplates(subj *chi.ClickHouseInstallation) {
 	// Create new target that will be populated with data during normalization process
-	n.req.SetTarget(n.createTarget())
+	n.req.SetTarget(n.newSubject())
 
 	// At this moment we have target available - it is either newly created or a system-wide template
 
 	// Apply CR templates - both auto and explicitly requested - on top of target
 	n.applyCRTemplatesOnTarget(subj)
 
-	// After all CR templates applied, place provided 'subject' on top of the whole stack (target)
-	n.req.GetTarget().MergeFrom(subj, chi.MergeTypeOverrideByNonEmptyValues)
+	// After all CR templates applied, place provided 'subject' on top of the whole target stack
+	n.applyCROnTarget(subj)
 }
 
-func (n *Normalizer) applyCRTemplatesOnTarget(subj crTemplatesNormalizer.TemplateSubject) {
-	usedTemplates := crTemplatesNormalizer.ApplyTemplates(n.req.GetTarget(), subj)
+func (n *Normalizer) applyCRTemplatesOnTarget(templateRefSrc crTemplatesNormalizer.TemplateRefListSource) {
+	usedTemplates := crTemplatesNormalizer.ApplyTemplates(n.req.GetTarget(), templateRefSrc)
 	n.req.GetTarget().EnsureStatus().PushUsedTemplate(usedTemplates...)
+}
+
+func (n *Normalizer) applyCROnTarget(cr *chi.ClickHouseInstallation) {
+	n.req.GetTarget().MergeFrom(cr, chi.MergeTypeOverrideByNonEmptyValues)
 }
 
 func (n *Normalizer) newSubject() *chi.ClickHouseInstallation {
@@ -121,24 +125,6 @@ func (n *Normalizer) ensureSubject(subj *chi.ClickHouseInstallation) *chi.ClickH
 	} else {
 		// Subject specified
 		return subj
-	}
-}
-
-func (n *Normalizer) getTargetTemplate() *chi.ClickHouseInstallation {
-	return chop.Config().Template.CHI.Runtime.Template
-}
-
-func (n *Normalizer) hasTargetTemplate() bool {
-	return n.getTargetTemplate() != nil
-}
-
-func (n *Normalizer) createTarget() *chi.ClickHouseInstallation {
-	if n.hasTargetTemplate() {
-		// Template specified - start with template
-		return n.getTargetTemplate().DeepCopy()
-	} else {
-		// No template specified - start with clear page
-		return n.newSubject()
 	}
 }
 
@@ -469,7 +455,7 @@ func (n *Normalizer) normalizeServiceTemplate(template *chi.ServiceTemplate) {
 
 // normalizeUseTemplates is a wrapper to hold the name of normalized section
 func (n *Normalizer) normalizeUseTemplates(templates []*chi.TemplateRef) []*chi.TemplateRef {
-	return crTemplatesNormalizer.NormalizeTemplatesList(templates)
+	return crTemplatesNormalizer.NormalizeTemplateRefList(templates)
 }
 
 // normalizeClusters normalizes clusters
