860f0fce87b282471ed811a490b41c1c9a7c0a4f
Slach <bloodjazman@gmail.com>
2025-06-21 17:01:33 +0400

    update grafana and prometheus, update grafana dashboards fix https://github.com/Altinity/clickhouse-operator/issues/1701

--- deploy/grafana/grafana-with-grafana-operator/install-grafana-operator.sh
+++ deploy/grafana/grafana-with-grafana-operator/install-grafana-operator.sh
@@ -4,7 +4,7 @@ echo "External value for \$GRAFANA_NAMESPACE=$GRAFANA_NAMESPACE"
 echo "External value for \$GRAFANA_OPERATOR_VERSION=$GRAFANA_OPERATOR_VERSION"
 
 GRAFANA_NAMESPACE="${GRAFANA_NAMESPACE:-grafana}"
-GRAFANA_OPERATOR_VERSION="${GRAFANA_OPERATOR_VERSION:-v4.8.0}"
+GRAFANA_OPERATOR_VERSION="${GRAFANA_OPERATOR_VERSION:-v5.18.0}"
 
 echo "Setup Grafana"
 echo "OPTIONS"
@@ -55,7 +55,7 @@ trap "clean_dir ${TMP_DIR}" SIGHUP SIGINT SIGQUIT SIGFPE SIGALRM SIGTERM
 
 # Continue with sources
 echo "Download Grafana operator sources into ${GRAFANA_OPERATOR_DIR}"
-git clone -b ${GRAFANA_OPERATOR_VERSION} --single-branch "https://github.com/grafana-operator/grafana-operator" "${GRAFANA_OPERATOR_DIR}"
+git clone -b ${GRAFANA_OPERATOR_VERSION} --single-branch "https://github.com/grafana/grafana-operator" "${GRAFANA_OPERATOR_DIR}"
 
 
 echo "Setup Grafana operator into ${GRAFANA_NAMESPACE} namespace"
@@ -65,19 +65,10 @@ echo "Setup Grafana operator into ${GRAFANA_NAMESPACE} namespace"
 kubectl create namespace "${GRAFANA_NAMESPACE}" || true
 
 # Setup grafana-operator into dedicated namespace
+sed -i "s/namespace: system/namespace: ${GRAFANA_NAMESPACE}/g" "${GRAFANA_OPERATOR_DIR}/deploy/kustomize/overlays/namespace_scoped/kustomization.yaml"
+kubectl kustomize "${GRAFANA_OPERATOR_DIR}/deploy/kustomize/overlays/namespace_scoped" --load-restrictor LoadRestrictionsNone | kubectl apply --server-side -f -
 
-# 1. Create the custom resource definitions that the operator uses:
-kubectl --namespace="${GRAFANA_NAMESPACE}" apply -f <(
- cat "${GRAFANA_OPERATOR_DIR}/deploy/manifests/${GRAFANA_OPERATOR_VERSION}/crds.yaml" | sed "s/namespace: system/namespace: ${GRAFANA_NAMESPACE}/g"
-)
-# 2. Create the operator roles:
-kubectl --namespace="${GRAFANA_NAMESPACE}" apply -f <(
-  cat "${GRAFANA_OPERATOR_DIR}/deploy/manifests/${GRAFANA_OPERATOR_VERSION}/rbac.yaml" | sed "s/namespace: system/namespace: ${GRAFANA_NAMESPACE}/g" | sed "s/controller-manager/grafana-operator/g"
-)
-# 3. Deploy the operator of explicitly specified version
-kubectl --namespace="${GRAFANA_NAMESPACE}" apply -f <(
-  cat "${GRAFANA_OPERATOR_DIR}/deploy/manifests/${GRAFANA_OPERATOR_VERSION}/deployment.yaml" | sed "s/namespace: system/namespace: ${GRAFANA_NAMESPACE}/g" | sed "s/controller-manager/grafana-operator/g"
-)
+kubectl wait deployment/grafana-operator-controller-manager -n "${GRAFANA_NAMESPACE}" --for=condition=available --timeout=300s
 
 # Remove downloaded sources
 clean_dir "${TMP_DIR}"
