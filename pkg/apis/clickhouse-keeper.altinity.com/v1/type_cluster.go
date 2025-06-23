f53a3f36e6020bd2305fd724eea5d1d77921a285
Vladislav Klimenko <sunsingerus@gmail.com>
2025-06-23 23:24:36 +0300

    dev: chk name accessor

--- pkg/apis/clickhouse-keeper.altinity.com/v1/type_cluster.go
+++ pkg/apis/clickhouse-keeper.altinity.com/v1/type_cluster.go
@@ -92,6 +92,14 @@ func (cluster *Cluster) GetName() string {
 	return cluster.Name
 }
 
+// HasName checks whether cluster has a name
+func (cluster *Cluster) HasName() bool {
+	if cluster == nil {
+		return false
+	}
+	return len(cluster.GetName()) > 0
+}
+
 func (c *Cluster) GetZookeeper() *apiChi.ZookeeperConfig {
 	return nil
 }
