ce136e8558375bfa52b9aa00b0d9ebd22c216b18
Vladislav Klimenko <sunsingerus@gmail.com>
2025-07-30 12:24:23 +0300

    dev: clarify error

--- pkg/controller/chi/worker-reconciler-chi.go
+++ pkg/controller/chi/worker-reconciler-chi.go
@@ -142,7 +142,7 @@ func (w *worker) buildCR(ctx context.Context, _cr *api.ClickHouseInstallation) *
 func (w *worker) buildCRFromObj(ctx context.Context, obj meta.Object) (*api.ClickHouseInstallation, error) {
 	_cr, err := w.c.GetCHI(obj)
 	if err != nil {
-		w.a.M(obj).F().Error("UNABLE-1 to find obj by %v err %v", obj.GetLabels(), err)
+		w.a.M(obj).F().Error("UNABLE-1 to find obj by labels: %v err: %v", obj.GetLabels(), err)
 		return nil, err
 	}
 	return w.buildCR(ctx, _cr), nil
