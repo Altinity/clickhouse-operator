320703a1488340ae5119ffaa156ab812747f9c45
Rohan Thakkar <rohant@twitter.com>
2026-07-17 12:31:01 -0700

    Fix informer delete handling
    Decode direct objects and DeletedFinalStateUnknown tombstones through one typed
    delete handler, reporting malformed events instead of panicking.
    
    Signed-off-by: Rohan Thakkar <rohant@twitter.com>

--- pkg/controller/chi/controller_test.go
+++ pkg/controller/chi/controller_test.go
@@ -6,10 +6,65 @@ import (
 	api "github.com/altinity/clickhouse-operator/pkg/apis/clickhouse.altinity.com/v1"
 	"github.com/altinity/clickhouse-operator/pkg/apis/common/types"
 	"github.com/altinity/clickhouse-operator/pkg/chop"
+	"k8s.io/client-go/tools/cache"
 )
 
 func init() {
 	chop.New(nil, nil, "")
+}
+
+func TestDeletedObject(t *testing.T) {
+	chi := &api.ClickHouseInstallation{}
+	var nilCHI *api.ClickHouseInstallation
+	tests := []struct {
+		name    string
+		obj     interface{}
+		want    *api.ClickHouseInstallation
+		wantErr bool
+	}{
+		{
+			name: "direct object",
+			obj:  chi,
+			want: chi,
+		},
+		{
+			name: "tombstone",
+			obj:  cache.DeletedFinalStateUnknown{Obj: chi},
+			want: chi,
+		},
+		{
+			name:    "unexpected direct object",
+			obj:     struct{}{},
+			wantErr: true,
+		},
+		{
+			name:    "unexpected tombstone object",
+			obj:     cache.DeletedFinalStateUnknown{Obj: struct{}{}},
+			wantErr: true,
+		},
+		{
+			name:    "nil tombstone object",
+			obj:     cache.DeletedFinalStateUnknown{},
+			wantErr: true,
+		},
+		{
+			name:    "typed nil tombstone object",
+			obj:     cache.DeletedFinalStateUnknown{Obj: nilCHI},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := deletedObject[api.ClickHouseInstallation](tt.obj)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("deletedObject() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if got != tt.want {
+				t.Errorf("deletedObject() = %p, want %p", got, tt.want)
+			}
+		})
+	}
 }
 
 func Test_shouldEnqueue(t *testing.T) {
